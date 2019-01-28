89bf38da3c2f07c9ccafeae1cdda365892d36f84
allekok <one@allekok.com>
2019-01-28 17:08:41 +0330

    remove an space

--- script/php/bk.php
+++ script/php/bk.php
@@ -56,7 +56,7 @@ $rid_k = num_convert($row['id'],"en","ckb");
     
 ?>
 <div style="display:flex;">
-    <button style="background:none;padding:0 .5em;" type="button" title="نیشان‌دانی بەشی سەرەتای ئەم شێعرە"><i class="material-icons" style="vertical-align: middle;font-size:1.5em;">keyboard_arrow_down</i></button><a href="/poet:<?php echo $ath; ?>/book:<?php echo $bk; ?>/poem:<?php echo $row['id']; ?>">
+    <button style="background:none;padding:0 .5em;" type="button" title="نیشان‌دانی بەشی سەرەتای ئەم شێعرە"><i class="material-icons" style="vertical-align:middle;font-size:1.5em;">keyboard_arrow_down</i></button><a href="/poet:<?php echo $ath; ?>/book:<?php echo $bk; ?>/poem:<?php echo $row['id']; ?>">
 <?php
 echo($rid_k . ". " . $row['name']);
 ?>
