d1776f2ef5b010fe8ede2e1486deb641df4fb681
allekok <one@allekok.com>
2019-01-28 16:50:44 +0330

    write some comments, https->http

--- script/php/constants-sample.php
+++ script/php/constants-sample.php
@@ -1,13 +1,12 @@
 <?php
-// rename this file to "constants.php"
 
-#mysql
+// mysql
 define("_HOST", "localhost");
 define("_USER","");
 define("_PASS",'');
 
-#site
-define("_SITE","https://".$_SERVER['HTTP_HOST']."/");
+// site
+define("_SITE","http://".$_SERVER['HTTP_HOST']."/"); // could be "https://..."
 define("_TITLE","ئاڵەکۆک");
 define("_DESC","شێعری شاعیرانی کورد");
 define("_KEYS","شیعر,هۆنراوە,کورد,کوردستان,شاعیرانی کورد,هەڵبەست,شعر,کرد,کردستان,شاعر,کوردی,allakok,alakok,alekok,ئاڵەکۆک,allekok,آلکوک,هەڵەکووک,هەڵەکوک");
