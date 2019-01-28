291fd465dd14f0f81533d672418fd9c812445bfb
allekok <one@allekok.com>
2019-01-28 19:13:27 +0330

    prevent cache for list.txt

--- pitew/save-pdfs-list.php
+++ pitew/save-pdfs-list.php
@@ -1,6 +1,7 @@
 <?php
 $base = "https://github.com/allekok/diwan/raw/master/";
-$list_uri = $base . "list.txt";
+$list_uri = $base . "list.txt?" . time();
 $list = @file_get_contents($list_uri);
 file_put_contents("pdfs.txt", $list);
+echo $list_uri;
 ?>
