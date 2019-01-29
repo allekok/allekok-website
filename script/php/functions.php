02087167f7111f42593c8b44544f9b0110a62581
allekok <one@allekok.com>
2019-01-29 03:56:03 +0330

    num_convert can now convert (en,ar,ckb) numbers to each other+some comments

--- script/php/functions.php
+++ script/php/functions.php
@@ -3,25 +3,25 @@
 require_once("constants.php");
 
 function num_convert($_num, $_from, $_to) {
+    // convert numbers from (en,ar,ckb) > (en,ar,ckb)
     
-	$_en = array('0','1','2','3','4','5','6','7','8','9');
-	$_ckb = array('۰','۱','۲','۳','۴','۵','۶','۷','۸','۹');
+	$_en = ["0","1","2","3","4","5","6","7","8","9"];
+    $_ar = ["٠", "١", "٢", "٣", "٤", "٥", "٦", "٧", "٨", "٩"];
+	$_ckb = ["۰","۱","۲","۳","۴","۵","۶","۷","۸","۹"];
 
-	if($_from === "en" && $_to === "ckb" ) {
+    $_assoc = ["en"=>$_en, "ar"=>$_ar, "ckb"=>$_ckb];
 
-		return str_replace($_en, $_ckb, $_num);
-	
-	} elseif($_from === "ckb" && $_to === "en") {
+    return str_replace($_assoc[$_from], $_assoc[$_to], $_num);
 
-		return str_replace($_ckb, $_en, $_num);
-	}
 }
 
 function get_poet_image($_pID, $_size, $_slash) {
-	$_sizes = array(
-		array("profile", "120x120"),
-		array("pro-460", "pro460", "460x460")
-	);
+    // returns poet's image uri or the default image.
+    
+	$_sizes = [
+		["pro-120", "profile", "120x120"],
+		["pro-460", "pro460", "460x460"]
+	];
 
 	$_pID = filter_var($_pID, FILTER_SANITIZE_NUMBER_INT);
 
@@ -50,10 +50,10 @@ function get_poet_image($_pID, $_size, $_slash) {
 }
 
 function color_num ($ath) {
+    // return color number for given poet's id.
     return ($ath%22) ? $ath - (22 * floor($ath/22)) : 22;
 }
 
 require_once("sanKuText.php");
-
 
 ?>
