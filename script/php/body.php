1bf707a78500a6d283b81afde8221b5a40e61374
allekok <one@allekok.com>
2019-01-29 03:57:03 +0330

    add some comments+remove unnecessary vars.

--- script/php/body.php
+++ script/php/body.php
@@ -1,11 +1,15 @@
 <?php
-    
-$title = _TITLE;
-$desc = _DESC;
-$keys = _KEYS;
-$t_desc = "";
 
-if( !empty($_GET['ath']) || !empty($_GET['bk']) || !empty($_GET['id']) || !empty($_GET['q']) ) {
+// default values
+$title = _TITLE; // page title used in <title></title>
+$desc = _DESC; // page description used in <meta name="description"> and <meta property="og:title">
+$keys = _KEYS; // page keywords used in <meta name="keywords">
+$t_desc = ""; // page description section used in "header h2" (only for index page till now)
+
+if( !empty($_GET['ath']) or !empty($_GET['bk']) or !empty($_GET['id']) or !empty($_GET['q']) ) {
+    // https://allekok.com/poet:{$ath}/book:{$bk}/poem:{$id}
+    // https://allekok.com/?ath={$ath}&bk={$bk}&id={$id}
+    // https://allekok.com/?q={$q}
     
     $ath = filter_var($_GET['ath'], FILTER_SANITIZE_NUMBER_INT);
     $bk = filter_var($_GET['bk'], FILTER_SANITIZE_NUMBER_INT);
@@ -15,7 +19,8 @@ if( !empty($_GET['ath']) || !empty($_GET['bk']) || !empty($_GET['id']) || !empty
     require('checkmurl.php');
 
 } elseif($_GET['job'] === "about") {
-    $about = 1;
+    // https://allekok.com/about
+    
     $color_num = 0;
     $title .= "؟"; 
     $desc = $title;
@@ -23,7 +28,8 @@ if( !empty($_GET['ath']) || !empty($_GET['bk']) || !empty($_GET['id']) || !empty
     include('about.php');
     
 } elseif($_GET['job'] === "thanks") {
-    $thanks = 1;
+    // https://allekok.com/thanks
+    
     $color_num = 0;
     $title = _TITLE . " &raquo; سپاس و پێزانین";
     $desc = $title;
@@ -31,6 +37,8 @@ if( !empty($_GET['ath']) || !empty($_GET['bk']) || !empty($_GET['id']) || !empty
     include('thanks.php');
     
 } else {
+    // https://allekok.com/
+    
     $t_desc = "<h2>"._DESC."</h2>";
     include('header.php');
     include('fbody.php');
