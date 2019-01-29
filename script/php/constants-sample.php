e171d322ed09e14fc670856c992ec23899752ed0
allekok <one@allekok.com>
2019-01-29 03:39:56 +0330

    move ABSPATH to the first line

--- script/php/constants-sample.php
+++ script/php/constants-sample.php
@@ -1,15 +1,16 @@
 <?php
 
+if(! defined('ABSPATH'))    define('ABSPATH', "/home/allekokc/public_html/");
+
 // mysql
 define("_HOST", "localhost");
 define("_USER","");
-define("_PASS",'');
+define("_PASS","");
 
 // site
-define("_SITE","http://".$_SERVER['HTTP_HOST']."/"); // could be "https://..."
+define("_SITE","http://".$_SERVER["HTTP_HOST"]."/"); // could be "https://..."
 define("_TITLE","ئاڵەکۆک");
 define("_DESC","شێعری شاعیرانی کورد");
 define("_KEYS","شیعر,هۆنراوە,کورد,کوردستان,شاعیرانی کورد,هەڵبەست,شعر,کرد,کردستان,شاعر,کوردی,allakok,alakok,alekok,ئاڵەکۆک,allekok,آلکوک,هەڵەکووک,هەڵەکوک");
-if(! defined('ABSPATH'))    define('ABSPATH', "/home/allekokc/public_html/");
 
 ?>
