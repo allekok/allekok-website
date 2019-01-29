948ff6989458d92e22841332d93ff844d9c88295
allekok <one@allekok.com>
2019-01-30 03:18:41 +0330

    correct bug: ignore contributors with no name

--- pitew/stats.php
+++ pitew/stats.php
@@ -1,19 +1,17 @@
 <?php
 
-    require_once("../script/php/functions.php");
+include_once("../script/php/functions.php");
 
-    if(isset($_GET['contributor'])) {
-        
-        $_name = filter_var($_GET['contributor'], FILTER_SANITIZE_STRING);
+if($_name = filter_var($_GET['contributor'], FILTER_SANITIZE_STRING)) {
 
-        $db = 'index';
-        $q = "SELECT * FROM pitew WHERE contributor='{$_name}' and status LIKE '{\"status\":1%'";
-        require("../script/php/condb.php");
+    $db = 'index';
+    $q = "SELECT id FROM pitew WHERE contributor='{$_name}' and status LIKE '{\"status\":1%'";
+    require("../script/php/condb.php");
         
-        if( mysqli_num_rows($query) > 0 ) {
+    if( mysqli_num_rows($query) > 0 ) {
             
-            echo num_convert(mysqli_num_rows($query),"en","ckb");
-        }
+        echo num_convert(mysqli_num_rows($query),"en","ckb");
     }
+}
 
 ?>
