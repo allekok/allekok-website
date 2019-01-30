cabc95fbd09c046b735c9e6dea71f552aa8296f4
allekok <one@allekok.com>
2019-01-31 02:07:10 +0330

    add comments, clear Input&output, close mysqli connection

--- pitew/append.php
+++ pitew/append.php
@@ -1,50 +1,62 @@
 <?php
+// add poem to "pitew" table in "index" database
+// INPUT: $_POST
+// OUTPUT: JSON
 
+// output: json
 header("Content-Type: application/json; charset=UTF-8");
 
-$contributor = filter_var($_POST['contributor'], FILTER_SANITIZE_STRING);
-$poet = filter_var($_POST['poet'], FILTER_SANITIZE_STRING);
-$poetDesc = filter_var($_POST['poetDesc'], FILTER_SANITIZE_STRING);
-$book = filter_var($_POST['book'], FILTER_SANITIZE_STRING);
-$poemName = filter_var($_POST['poemName'], FILTER_SANITIZE_STRING);
-$poem = filter_var($_POST['poem'], FILTER_SANITIZE_STRING);
+/* INPUT */
 
-if(empty($poet) || empty($poem)) {
-    
-    // state, message, contributor['name'],['ID']
-    $res = array(
+// Required
+$poet = filter_var($_POST['poet'], FILTER_SANITIZE_STRING); // poet's name
+$poem = filter_var($_POST['poem'], FILTER_SANITIZE_STRING); // poem itself
+
+// Optional
+$contributor = filter_var($_POST['contributor'], FILTER_SANITIZE_STRING); // contributor name
+$poetDesc = filter_var($_POST['poetDesc'], FILTER_SANITIZE_STRING); // poet's description for new poets
+$book = filter_var($_POST['book'], FILTER_SANITIZE_STRING); // book's name
+$poemName = filter_var($_POST['poemName'], FILTER_SANITIZE_STRING); // poem's name
+
+/* INPUT */
+
+/* OUTPUT */
+// ["state","message","contributor"=>["name","id"]]
+/* OUTPUT */
+
+if(empty($poet) or empty($poem)) {    
+    $res = [
         "state"=>0,
-        "message"=>"<i class='nr'>تکایە ناوی شاعیر و شێعرەکەی بنووسن.</i>");
-        
-    echo( json_encode($res) );
-} else {
-    
-    $date = date("Y-m-d_h:i:sa");
-    $IP = $_SERVER['REMOTE_ADDR'];
-    $nfo = $date . " --- " . $IP;
-    
-    // status-->0,1,-1 & if 1:url & desc
-    $status = array("status"=>0, "url"=>"", "desc"=>"");
-    $status = json_encode($status);
-    
-    $db = "index";
-    $q = "INSERT INTO `pitew`(`contributor`, `poet`, `book`, `poem-name`, `poem`, `date`, `status`, `poetDesc`) VALUES ('$contributor', '$poet', '$book', '$poemName', '$poem', '$nfo', '$status','$poetDesc')";
-    require_once("../script/php/condb.php");
-    
-    if($query) {
-        
-        $res = array(
-            "state"=>1,
-            "message"=>"<i class='ng'>زۆر سپاس بۆ ئێوە. ئەو شێعرە دوای پێداچوونەوە لەسەر ئاڵەکۆک دادەندرێ.</i>",
-            "contributor"=> array(
-                "name"=>$contributor,
-                "ID"=>""
-                )
-            );
-        
-        echo(json_encode($res));
-        
-    }
+        "message"=>"<i class='nr'>تکایە ناوی شاعیر و شێعرەکەی بنووسن.</i>"];
+    die( json_encode($res) );    
 }
+
+$date = date("Y-m-d_h:i:sa");
+$IP = $_SERVER['REMOTE_ADDR'];
+$nfo = $date . " --- " . $IP;
+
+// status-->0,1,-1 & if 1:url & desc
+$status = ["status"=>0, "url"=>"", "desc"=>""];
+$status = json_encode($status);
+
+$db = "index";
+$q = "INSERT INTO `pitew`(`contributor`, `poet`, `book`, `poem-name`, `poem`, `date`, `status`, `poetDesc`) VALUES ('$contributor', '$poet', '$book', '$poemName', '$poem', '$nfo', '$status','$poetDesc')";
+require_once("../script/php/condb.php");
+
+if($query) {
+    
+    $res = [
+        "state"=>1,
+        "message"=>"<i class='ng'>زۆر سپاس بۆ ئێوە. ئەو شێعرە دوای پێداچوونەوە لەسەر ئاڵەکۆک دادەندرێ.</i>",
+        "contributor"=> [
+            "name"=>$contributor,
+            "ID"=>""
+        ],
+    ];
+    
+    echo(json_encode($res));
+}
+
+mysqli_close($conn);
 
 ?>
