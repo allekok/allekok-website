9c18f0981f48ae7a5e8ef7b5b9efd14dc81a00fb
allekok <one@allekok.com>
2019-01-31 01:01:17 +0330

    order poets by id (script/php/fbody.phpGET[order])

--- script/php/fbody.php
+++ script/php/fbody.php
@@ -1,41 +1,41 @@
 <?php
 
-$__isnew = isset($_GET['new']) ? " WHERE kind='alive'" : " WHERE kind='dead'";
+$db = "index";
 
-$db = 'index';
+$__isnew = isset($_GET["new"]) ? " WHERE kind='alive'" : " WHERE kind='dead'";
 
-$__order = " ORDER BY takh ASC";
-    
+$__order = (@$_GET["order"]=="id") ? " ORDER BY id ASC" : " ORDER BY takh ASC";
+
 $q = "SELECT id,profname,takh FROM auth" . $__isnew . $__order ;
-require('condb.php');
+require("condb.php");
 
 ?>
 
 <div id='poets'>
     <div style="animation:tL .8s;padding:0 0 1em;">
         <?php if(isset($_GET['new'])) { ?>
-        <span style="display: block;font-size: .55em;color: #444;padding: 1em;background-color: #f3f3f3;margin-top: 1em;">
-          مەبەست لە شاعیرانی نوێ، ئەو شاعیرانەن کە لە ژیان دا ماون.
-        </span>
+            <span style="display: block;font-size: .55em;color: #444;padding: 1em;background-color: #f3f3f3;margin-top: 1em;">
+		مەبەست لە شاعیرانی نوێ، ئەو شاعیرانەن کە لە ژیان دا ماون.
+            </span>
         <?php } ?>
-<?php
-    
-if($query) {
+	<?php
+	
+	if($query) {
 
-while($row=mysqli_fetch_assoc($query)) {
+	    while($row=mysqli_fetch_assoc($query)) {
 
-    $imgsrc = get_poet_image($row['id'], "profile", 1);
-?>
-<section class="psec"><a href="/poet:<?php echo $row['id'] ?>"><img alt="<?php echo $row['profname']; ?>" src="<?php echo $imgsrc; ?>"><h3 title="<?php echo $row['profname']; ?>"><?php  echo $row['takh'];  ?></h3></a></section><?php
-}
+		$imgsrc = get_poet_image($row['id'], "profile", 1);
+	?>
+	    <section class="psec"><a href="/poet:<?php echo $row['id'] ?>"><img alt="<?php echo $row['profname']; ?>" src="<?php echo $imgsrc; ?>"><h3 title="<?php echo $row['profname']; ?>"><?php  echo $row['takh'];  ?></h3></a></section><?php
+																													       }
 
-}
-mysqli_close($conn);
+																													       }
+																													       mysqli_close($conn);
 
-?>
-</div><div style="padding: 1em 0 0;border-top: 2px dashed #eee;">
+																													       ?>
+    </div><div style="padding: 1em 0 0;border-top: 2px dashed #eee;">
         <?php if(isset($_GET['new'])) { ?>
-        <a class='button' style='display: inline-block;margin: 0 0 0 1em;' href="/">شاعیرانی کۆچ‌کردوو</a><?php } else { ?><a class='button' style='display: inline-block;margin: 0 0 0 1em;' href="/?new">شاعیرانی نوێ</a><?php } ?><a class='button' style='display: inline-block;margin: 0 1em 0 0;' href="/poet:73">بەیتی کوردی</a>
+            <a class='button' style='display: inline-block;margin: 0 0 0 1em;' href="/">شاعیرانی کۆچ‌کردوو</a><?php } else { ?><a class='button' style='display: inline-block;margin: 0 0 0 1em;' href="/?new">شاعیرانی نوێ</a><?php } ?><a class='button' style='display: inline-block;margin: 0 1em 0 0;' href="/poet:73">بەیتی کوردی</a>
     </div>
 
 
