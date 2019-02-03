f851ad9686745dcb01ca72a4cb9ccf6fe96c6c38
allekok <one@allekok.com>
2019-02-03 15:27:09 +0330

    change تەوار -> تازەکان

--- script/php/footer.php
+++ script/php/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <a href="/desktop/"><i class='material-icons' style='vertical-align:middle'>laptop</i></a><a href="/mobile/"><i class='material-icons' style='vertical-align:middle'>phone_iphone</i></a><a href="/about">ئاڵەکۆک؟</a><a href="/pitew/first.php">پتەوکردن</a><a href="/tewar/">تەوار</a><a href="/thanks">سپاس</a><a href="/dev/tools/"><i class='material-icons' style='vertical-align:middle'>code</i></a><a title="چۆنیەتی بەکارهێنانی ئاڵەکۆک" href="/manual/"><i class='material-icons' style='vertical-align:middle'>help</i></a>
+    <a href="/desktop/"><i class='material-icons' style='vertical-align:middle'>laptop</i></a><a href="/mobile/"><i class='material-icons' style='vertical-align:middle'>phone_iphone</i></a><a href="/about">ئاڵەکۆک؟</a><a href="/pitew/news.php">تازەکان</a><a href="/pitew/first.php">پتەوکردن</a><a href="/thanks">سپاس</a><a href="/dev/tools/"><i class='material-icons' style='vertical-align:middle'>code</i></a><a title="چۆنیەتی بەکارهێنانی ئاڵەکۆک" href="/manual/"><i class='material-icons' style='vertical-align:middle'>help</i></a>
 </footer>
 
 <script async src="/script/js/main.js?v3"></script>
