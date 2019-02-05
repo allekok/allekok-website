565c756ca10cc1e9186d408f8312a9f71ec4b7d6
allekok <one@allekok.com>
2019-02-05 03:31:58 +0330

    shorting the way of adding comments

--- script/php/comments-add.php
+++ script/php/comments-add.php
@@ -1,87 +1,57 @@
 <?php
+// add poem's comments to database.
 
-    require_once("functions.php");
-    
-    $_comment = filter_var($_REQUEST['comment'], FILTER_SANITIZE_STRING);
+include_once("functions.php");
 
-    if(empty($_comment))    die();
+$_comment = @filter_var($_POST["comment"], FILTER_SANITIZE_STRING);
+if(empty($_comment)) die();
 
+$__address = isset($_POST["address"]) ? filter_var($_POST["address"], FILTER_SANITIZE_STRING) : die();
+
+$address =  explode("/",$__address);
+
+$address = array_map("explde", $address);
+
+function explde($in) {
+    return explode(":", $in);
+}
+
+$db = "index";
+$tbl = "tbl{$address[0][1]}_{$address[1][1]}";
+$q = "select id from {$tbl} where id={$address[2][1]}";
+include("condb.php");
+
+if($query) {
+    if(! ( mysqli_num_rows($query) > 0 ) ) die();
+}
+
+$date = date("Y-m-d h:i:sa");
+$_date = $date . " " . $_SERVER['REMOTE_ADDR'];
+
+$_name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+
+
+$q = "INSERT INTO comments(address, date, name, comment) VALUES('$__address', '$_date', '$_name', '$_comment')";
+$query = mysqli_query($conn, $q);
+
+if($query) {
     
-    $address = isset($_REQUEST['address']) ? explode("/", $_REQUEST['address']) : die('');
+    $date = num_convert($date, "en", "ckb");
+    $date = str_replace(["am","pm"], [" بەیانی "," پاش‌نیوەڕۆ "], $date);
     
-    $address = array_map("explde", $address);
+    if($_name == "") $_name = "ناشناس";
     
-    function explde($in) {
-        return explode(":", $in);
-    }
+    $res = [
+        "message"=>"",
+        "status"=>1,
+        "name"=>$_name,
+        "comment"=>$_comment,
+        "date"=>$date,
+    ];
     
-    $db = 'index';
-    $q = 'select id,bks from auth order by id ASC';
-    require("condb.php");
-    
-    if($query) {
-        
-        if( (mysqli_num_rows($query)+1) >= $address[0][1] ) {
-            
-            while($res = mysqli_fetch_assoc($query)) {
-                
-                if($res['id'] == $address[0][1]) {
-                    
-                    $res['bks'] = explode(',', $res['bks']);
-                    if(! (count($res['bks']) >= $address[1][1]) ) {
-                        
-                        die("");
-                    }
-                
-                }
-                
-            }
-            
-            $q = "select id from tbl{$address[0][1]}_{$address[1][1]} where id=$address[2][1]";
-            $query = mysqli_query($conn, $q);
-            
-            if($query) {
-                if(! ( mysqli_num_rows($query) > 0 ) ) {
-                    
-                    die("");
-                }
-            }
-        }
-        
-    }
-    
-    $date = date("Y-m-d h:i:sa");
-    
-    $_date = $date . " " . $_SERVER['REMOTE_ADDR'];
-    
-    $_name = filter_var($_REQUEST['name'], FILTER_SANITIZE_STRING);
-    
-    $__address = implode(":", $address[0]) . "/" . implode(":", $address[1]) . "/" . implode(":", $address[2]);
-    
-    
-    $q = "INSERT INTO comments(address, date, name, comment) VALUES('$__address', '$_date', '$_name', '$_comment')";
-    
-    $query = mysqli_query($conn, $q);
-    
-    if($query) {
-        
-        $date = num_convert($date, "en", "ckb");
-                
-        $date = str_replace(["am","pm"], [" بەیانی "," پاش‌نیوەڕۆ "], $date);
-        
-        if($_name == "")    $_name = "ناشناس";
-                
-        $res = array(
-            "message"=>"",
-            "status"=>1,
-            "name"=>$_name,
-            "comment"=>$_comment,
-            "date"=>$date,
-            );
-            
-        echo json_encode($res);
-    }
-    
-    mysqli_close($conn);
-    
+    echo json_encode($res);
+}
+
+mysqli_close($conn);
+
 ?>
