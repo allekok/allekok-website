d861070db85e7b9f3da3667140e0b862cea1dbfa
allekok <one@allekok.com>
2019-02-15 17:51:08 +0330

    remove inside fetch event in sw.js

--- sw.js
+++ sw.js
@@ -99,11 +99,6 @@ self.addEventListener('fetch', function(event) {
     event.respondWith(
 	caches.match(event.request).then(function(resp) {
 	    return resp || fetch(event.request).then(function(response) {
-		//let responseClone = response.clone();
-		//    caches.open(cache_ver).then(function(cache) {
-		//      cache.put(event.request, responseClone);
-		//    });
-		
 		return response;
 	    });
 	}).catch(function() {
