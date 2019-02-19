31ed4e67ec0068bae109fa21d885426cfec9d09d
allekok <one@allekok.com>
2019-02-19 17:47:17 +0330

    restyling constants-sample.php file

--- script/php/constants-sample.php
+++ script/php/constants-sample.php
@@ -1,19 +1,26 @@
 <?php
 
-if(! defined('ABSPATH'))    define('ABSPATH', "/");
+/** Absolute path **/
+if(! defined("ABSPATH"))
+    define("ABSPATH", "/");
 
-// mysql
+/** Mysql configuration **/
 define("_HOST", "localhost");
 define("_USER","");
 define("_PASS","");
-define("_DEFAULT_DB",""); // default database name without prefix
+define("_DEFAULT_DB","");
+// default database name without prefix
 define("_DB_PREFIX","");
-// this constant is used in "condb.php". -> Database name = _DB_PREFIX . $db;
+// Database prefixes, this constant is used in "condb.php". -> Database name = _DB_PREFIX . $db;
 
-// site
-define("_SITE","http://".$_SERVER["HTTP_HOST"]."/"); // could be "https://..."
+/** Site constants **/
+define("_SITE",
+       "http://".$_SERVER["HTTP_HOST"]."/");
+// could be "https://..."
 define("_TITLE","ئاڵەکۆک");
-define("_DESC","شێعری شاعیرانی کورد");
-define("_KEYS","شیعر,هۆنراوە,کورد,کوردستان,شاعیرانی کورد,هەڵبەست,شعر,کرد,کردستان,شاعر,کوردی,allakok,alakok,alekok,ئاڵەکۆک,allekok,آلکوک,هەڵەکووک,هەڵەکوک");
+define("_DESC",
+       "شێعری شاعیرانی کورد");
+define("_KEYS",
+       "شیعر,هۆنراوە,کورد,کوردستان,شاعیرانی کورد,هەڵبەست,شعر,کرد,کردستان,شاعر,کوردی,allakok,alakok,alekok,ئاڵەکۆک,allekok,آلکوک,هەڵەکووک,هەڵەکوک");
 
 ?>
