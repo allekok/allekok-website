803786bb53eee92af627cd59407f34594c5ba2db
allekok <one@allekok.com>
2019-03-10 20:52:15 +0330

    rewrite about-comments.php file(correct styling, just make it better)

--- about/about-comments.php
+++ about/about-comments.php
@@ -1,27 +1,28 @@
 <?php
 
-/// *** Reading comments in {$uri} file and print them.
+/* Reading comments in {$uri} file and print them. */
 
 $uri = "res/about.comments";
 
-if(file_exists($uri) and $fs = filesize($uri)) {
-
-    $f = fopen($uri, "r");
+if(file_exists($uri)) {
     
-    $content = fread($f,$fs);
-    $conts = explode("[comment]",$content);
-    $conts = array_reverse($conts);
-    if( false == filter_var(@$_GET['num'], FILTER_VALIDATE_INT) )    $_GET['num'] = -1;    
+    $comments = file_get_contents($uri);
+    $comments = explode("[comment]",$comments);
+    $comments = array_reverse($comments);
+
+    $limit = (false === filter_var(@$_GET['num'],
+		   FILTER_VALIDATE_INT)) ? -1 :
+	     $_GET['num'];
     $_n = 0;
-    foreach($conts as $c) {
-        if($_n == $_GET['num']) break 1;
+    foreach($comments as $c) {
+        if($_n == $limit) break;
         
-        if(!@$_GET["plain"]) $c = str_replace("\n", "<br>", $c);
+        if(!@$_GET["plain"])
+	    $c = str_replace("\n", "<br>", $c);
         
         echo $c;
         $_n++;
     }
-    
-    fclose($f);
 }
+
 ?>
