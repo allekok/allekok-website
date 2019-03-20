31d5584b03a13eb5150b66bf562d83c9cffc9f06
allekok <one@allekok.com>
2019-03-21 02:43:49 +0330

    close mysql connection before redirection

--- script/php/UpdateCipi.php
+++ script/php/UpdateCipi.php
@@ -23,11 +23,11 @@ if(mysqli_num_rows($query)===1) {
     $Cipi = $C / $imp;
     
     $query = mysqli_query($conn,"UPDATE `poems` SET `C`=$C, `Cipi`=$Cipi WHERE poet_address='$address[0]' and book_address='$address[1]' and poem_address='$address[2]'");
+
+    mysqli_close($conn);
     
     if($query)
         redirect(_SITE.$uri);
 }
-
-mysqli_close($conn);
 
 ?>
