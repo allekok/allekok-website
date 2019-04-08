3746520e9bd24afb1e0a0285e8ddc0df82195736
allekok <one@allekok.com>
2019-04-09 03:08:05 +0430

    add pitew/contributors/, ignore cont../*.txt

---
+++ pitew/contributors/index.php
@@ -0,0 +1,129 @@
+<?php
+
+include_once("../../script/php/constants.php");
+include_once(ABSPATH . "script/php/colors.php");
+include_once(ABSPATH . "script/php/functions.php");
+
+$title = _TITLE . " &rsaquo; پتەوکردنی ئاڵەکۆک &raquo; بەشداربووان";
+$desc = "بەشداربووان و یارمەتیدەرانی ئاڵەکۆک";
+$keys = _KEYS;
+$t_desc = "";
+$color_num = 0;
+
+include(ABSPATH . "script/php/header.php");
+?>
+
+<div id="poets" style="margin-bottom:4em;max-width:800px">
+    <div id='adrs'>
+	<a href="../first.php">
+	    <i style='vertical-align:middle;color:transparent;border-radius:100%;border:2px dashed #aaa;' class='material-icons'>person</i> پتەوکردنی ئاڵەکۆک
+	</a>
+	<i style='font-style:normal;'> &rsaquo; </i>
+	<div id='current-location'>
+	    <i style='vertical-align:middle;' class='material-icons'>favorite</i>
+	    بەشداربووان
+	</div>
+    </div>
+
+    <style>
+     #contributions {
+	 font-size:.6em;
+     }
+     #contributions div p {
+	 text-align:right;
+     }
+     #contributions div .stats-min {
+	 display:block;
+	 font-size:.9em;
+     }
+     #contributions div .stats-min .material-icons {
+	 font-size:.85em;
+	 vertical-align:middle;
+	 padding:0 .2em;
+	 display:inline-block;
+     }
+     .epld-expand {
+	 font-size:1em;
+	 padding:.5em 1em;
+	 margin:1em 0;
+	 background:#666;
+	 color:#fff;
+     }
+     .epld-expand i {
+	 vertical-align:middle;
+     }
+    </style>
+    <script>
+     function expand(item) {
+	 var parent = item.
+		   parentNode.
+		     parentNode.querySelector("div");
+	 if(parent.style.overflow != "hidden") {
+	     parent.style.overflow = "hidden";
+	     parent.style.maxHeight = "400px";
+	     item.innerHTML = "<i class='material-icons' style='font-size:inherit;display:inline-block'>keyboard_arrow_down</i>";
+	 } else {
+	     parent.style.overflow = "";
+	     parent.style.maxHeight = "";
+	     item.innerHTML = "<i class='material-icons' style='font-size:inherit;display:inline-block'>keyboard_arrow_up</i>";	     
+	 }
+     }
+    </script>
+    
+    <div id="contributions">
+	<?php
+	function open($uri, $limit=-2) {
+	    $f = fopen($uri, 'r');
+	    $array = [];
+	    $n = -1;
+	    while(! feof($f)) {
+		if($limit === $n) break;
+		$line = fgets($f);
+		if(!trim($line)) continue;
+		$array[] = explode("\t", $line);
+		$n++;
+	    }
+	    return $array;
+	}
+	?>
+	<?php
+	$array = [
+	    ['poems', 'نووسینی شێعر', 'poems.txt', 'note_add'],
+	    ['images', 'وێنەی شاعیران', 'images.txt', 'image'],
+	    ['poet-descs', 'نووسینی زانیاری سەبارەت بە شاعیران', 'poet-descs.txt', 'person'],
+	    ['comments', 'ڕاست‌کردنەوەی هەڵەکانی ناو شێعر', 'comments.txt', 'question_answer'],
+	    ['pdfs', 'ناردنی دیوانی شاعیران', 'pdfs.txt', 'cloud_download'],
+	];
+
+	foreach($array as $E) {
+	    echo "<div id='contributions-{$E[0]}' class='pitewsec'>";
+	    echo "<div style='overflow:hidden;max-height:400px'><i class='material-icons'>{$E[3]}</i>";
+	    echo '<h2>';
+	    echo $E[1];
+	    echo '</h2>';
+	    $contributions_poems = open($E[2]);
+	    echo '<i class=\'stats-min\'>';
+	    echo "<a href='{$E[2]}' title='وەشانی plain/text'><i class='material-icons'>insert_drive_file</i></a>";
+	    echo 'ئەژمار: ';
+	    echo num_convert(
+		number_format(
+		    array_shift($contributions_poems)[0]),'en','ckb');
+	    echo '</i>';
+	    foreach($contributions_poems as $k=>$e) {
+		echo '<p>';
+		echo num_convert(($k+1).'. '.$e[1], 'en', 'ckb');
+		echo '</p>';
+	    }
+	    echo "</div><div><button class='epld-expand button' onclick='expand(this)'><i class='material-icons' style='font-size:inherit;display:inline-block'>keyboard_arrow_down</i></button></div>";
+	    echo '</div>';
+	}
+
+	?>
+    </div>
+
+</div>
+
+
+<?php
+include_once(ABSPATH."script/php/footer.php");
+?>
---
+++ pitew/contributors/make-lists.php
@@ -0,0 +1,133 @@
+<?php
+function poem_writers () {
+    $q = 'SELECT contributor FROM pitew WHERE status LIKE \'{\"status\":1%\'';
+    require('../../script/php/condb.php');
+    $writers = [];
+    while($res = mysqli_fetch_assoc($query)) {
+	if(! $res['contributor'])
+	    continue;
+	$name = $res['contributor'];
+	if(isset($writers[$name]))
+	    $writers[$name][0] += 1;
+	else
+	    $writers[$name] = [1, $name];
+    }
+    mysqli_close($conn);
+    rsort($writers);
+    return $writers;
+}
+
+function image_contributors () {
+    $dir = opendir('../../style/img/poets/new/');
+    $ignore = ['.','..'];
+    $contributors = [];
+    while(false !== ($e=readdir($dir))) {
+	if(in_array($e, $ignore))
+	    continue;
+
+	$name = explode('_', str_replace(['.jpeg', '.png'], '', $e))[1];
+	if(isset($contributors[$name]))
+	    $contributors[$name][0] += 1;
+	else
+	    $contributors[$name] = [1, $name];
+    }
+    closedir($dir);
+    rsort($contributors);
+    return $contributors;
+}
+
+function poet_description_writers () {
+    $dir = opendir('../res/');
+    $ignore = ['.','..'];
+    $writers = [];
+    while(false !== ($e=readdir($dir))) {
+	if(in_array($e, $ignore))
+	    continue;
+
+	$name = explode('_',$e)[0];
+	if(isset($writers[$name]))
+	    $writers[$name][0] += 1;
+	else
+	    $writers[$name] = [1, $name];
+    }
+    closedir($dir);
+    rsort($writers);
+    return $writers;
+}
+
+function comment_contributors () {
+    $q = 'SELECT name FROM comments WHERE blocked=0 AND `read`=1';
+    require('../../script/php/condb.php');
+    $contributors = [];
+    while($res = mysqli_fetch_assoc($query)) {
+	if(! $res['name'])
+	    continue;
+	$name = $res['name'];
+	if(isset($contributors[$name]))
+	    $contributors[$name][0] += 1;
+	else
+	    $contributors[$name] = [1, $name];
+    }
+    mysqli_close($conn);
+    rsort($contributors);
+    return $contributors;
+}
+
+function pdf_contributors () {
+    $pdfs = explode("\n\n",
+		    trim(file_get_contents('../pdfs.txt')));
+    $needle = 'ناردن: ';
+    $contributors = [];
+    
+    foreach($pdfs as $pdf) {
+	$pdf = explode("\t\t",$pdf);
+	$last_line = @array_pop(
+	    explode("\n",$pdf[2]));
+	
+	if(strpos($last_line,$needle) !== 0)
+	    continue;
+
+	$name = substr($last_line,strlen($needle));
+	if(@isset($contributors[$name])) 
+	    $contributors[$name][0] += 1;
+	else
+	    $contributors[$name] = [1, $name];
+    }
+    
+    rsort($contributors);
+    return $contributors;
+}
+
+function number_of_words($string) {
+    preg_match_all("/\w+/", $string, $string);
+    return count($string[0]);
+}
+
+function sum ($array) {
+    $sum = 0;
+    foreach($array as $e) {
+	$sum += $e[0];
+    }
+    return $sum;
+}
+
+function save ($uri, $array) {
+    $f = fopen($uri, 'w');
+    fwrite($f, sum($array) . "\t*\n");
+    foreach($array as $e) {
+	fwrite($f, implode("\t", $e) . "\n");
+    }
+    fclose($f);
+}
+
+function save_all () {
+    save('poems.txt', poem_writers());
+    save('images.txt', image_contributors());
+    save('poet-descs.txt', poet_description_writers());
+    save('comments.txt', comment_contributors());
+    save('pdfs.txt', pdf_contributors());
+}
+
+save_all();
+
+?>
