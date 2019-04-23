4cbe1fb2b0b6a0210715dc8635a8f271ab59aabf
allekok <one@allekok.com>
2019-04-24 01:32:53 +0430

    [main.css?v19] Update version

--- script/php/header.php
+++ script/php/header.php
@@ -23,7 +23,7 @@ if(isset($ath)) {
 	<script>
          if ('serviceWorker' in navigator) navigator.serviceWorker.register('/sw.js', {scope: '/'});
 	</script>
-	<link rel='stylesheet' href='/style/css/main.css?v18' />
+	<link rel='stylesheet' href='/style/css/main.css?v19'/>
 	<title>
 	    <?php echo($title); ?>
 	</title>
