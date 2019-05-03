18a43d033d95ec8fc09e9c86bf96b1ef1f4fa60a
allekok <one@allekok.com>
2019-05-03 12:50:10 +0430

    [*/save.php,pitew/save-comment.php] General optimizations: Check for $_POST['txt'] before using it.

--- mobile/save.php
+++ mobile/save.php
@@ -1,13 +1,12 @@
 <?php
-
-$t = htmlspecialchars($_POST['txt']);
-$t = stripslashes($t);
-if(empty($t))   die();
+$t = isset($_POST['txt']) ?
+     trim(stripslashes(
+	 htmlspecialchars(
+	     $_POST['txt']))) : die();
+if(empty($t)) die();
 
 $f = fopen("QA.txt", "a");
-fwrite($f, $t . "\nend\n");
+fwrite($f, $t."\nend\n");
 fclose($f);
-
 echo "1";
-
 ?>
