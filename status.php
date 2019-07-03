6acd1fb0a18e97202491cbc9800339ffc69653c8
allekok <one@allekok.com>
2019-07-03 15:00:39 +0430

    (status.php) Update styles

--- status.php
+++ status.php
@@ -1,7 +1,8 @@
 <?php
-header("Content-type:text/plain; charset=UTF-8");
 require_once("script/php/constants.php");
 require_once(ABSPATH."script/php/functions.php");
+
+header("Content-type:text/plain; charset=UTF-8");
 
 /* Statistics */
 include(ABSPATH."script/php/stats.php");
@@ -13,7 +14,7 @@ echo "\tشێعر:";
 echo $hons_num;
 
 /* New-Poems */
-echo "\n\n*نووسینی شێعر*\n";
+echo "\n\n/نووسینی شێعر/\n";
 $db = "index";
 $q = "select * from pitew where status LIKE '{\"status\":0%' order by id DESC";
 require(ABSPATH."script/php/condb.php");
@@ -22,16 +23,84 @@ if($query and (mysqli_num_rows($query) > 0))
     while($res = mysqli_fetch_assoc($query))
     {
 	$res['poem-name'] = $res['poem-name'] ? $res['poem-name'] : "شێعر";
-	echo "• {$res['contributor']}:: {$res['poet']} › {$res['book']} › {$res['poem-name']}\n";
+	echo "- {$res['contributor']} :: {$res['poet']} › {$res['book']} › {$res['poem-name']}\n";
     }
 }
 else
 {
-    echo "•\n";
+    echo "+•+\n";
 }
 
 /* New-Images */
-echo "\n*ناردنی وێنەی شاعیران*\n";
+echo "/ناردنی وێنەی شاعیران/\n";
+$_list = make_list(ABSPATH."style/img/poets/new/");
+foreach($_list as $i=>$_l)
+{
+    if($i == 2) break;
+    echo "- {$_l['name']} › {$_l['poet']}\n";
+}
+if(!$_list) echo "+•+\n";
+
+/* New-Poet-Descs */
+echo "/نووسینی زانیاری سەبارەت بە شاعیران/\n";
+$_list = make_list(ABSPATH."pitew/res/");
+foreach($_list as $i=>$_l)
+{
+    if($i == 2)	break;
+    echo "- {$_l['poet']} › {$_l['name']}\n";
+}
+if(!$_list) echo "+•+\n";
+
+/* New-Comments */
+echo "/بیر و ڕای شێعرەکان/\n";
+$q = "select * from `comments` where `read`=0 order by `id` DESC";
+$query = mysqli_query($conn, $q);
+if($query and (mysqli_num_rows($query) > 0))
+{
+    while($res = mysqli_fetch_assoc($query))
+    {
+	$res['name'] = $res['name'] ? $res['name'] : "ناشناس";
+	echo "- {$res['name']} › https://allekok.com/{$res['address']}\n";
+    }
+}
+else
+{
+    echo "+•+\n";
+}
+mysqli_close($conn);
+
+/* about */
+echo "/about/\n";
+echo filter_var(
+    file_get_contents(
+	'https://allekok.com/about/about-comments.php?num=1'),
+    FILTER_SANITIZE_STRING) . "\n";
+
+/* allekok-desktop */
+echo "/allekok-desktop/\n";
+echo last(ABSPATH . "desktop/QA.txt") . "\n";
+
+/* allekok-mobile */
+echo "/allekok-mobile/\n";
+echo last(ABSPATH . "mobile/QA.txt") . "\n";
+
+/* pitew */
+echo "/پتەوکردنی ئاڵەکۆک/\n";
+echo last(ABSPATH . "pitew/QA.txt") . "\n";
+
+/* dev/tools */
+echo "/dev/\n";
+echo last(ABSPATH . "dev/tools/QA.txt") . "\n";
+
+/* manual */
+echo "/manual/\n";
+echo last(ABSPATH . "manual/QA.txt") . "\n";
+
+/* CONTRIBUTING */
+echo "/CONTRIBUTING/\n";
+echo last(ABSPATH . "dev/tools/CONTRIBUTING/QA.txt") . "\n";
+
+/* Tools */
 function make_list($_dir)
 {
     if(!is_dir($_dir)) return [];
@@ -53,68 +122,27 @@ function make_list($_dir)
     rsort($_list);
     return $_list;
 }
-$_list = make_list(ABSPATH."style/img/poets/new/");
-foreach($_list as $i=>$_l)
-{
-    if($i == 2) break;
-    echo "• {$_l['name']}:: {$_l['poet']}\n";
-}
-if(!$_list) echo "•\n";
 
-/* New-Poet-Descs */
-echo "\n*نووسینی زانیاری سەبارەت بە شاعیران*\n";
-$_list = make_list(ABSPATH."pitew/res/");
-foreach($_list as $i=>$_l)
+function last ($path, $n=1)
 {
-    if($i == 2)	break;
-    echo "• {$_l['poet']}:: {$_l['name']}\n";
-}
-if(!$_list) echo "•\n";
-
-/* New-Comments */
-echo "\n*بیر و ڕای شێعرەکان*\n";
-$q = "select * from `comments` where `read`=0 order by `id` DESC";
-$query = mysqli_query($conn, $q);
-if($query and (mysqli_num_rows($query) > 0))
-{
-    while($res = mysqli_fetch_assoc($query))
+    /* Get 'n' items from the tail of a 'QA.txt' */
+    if(! file_exists($path))
+	return false;
+    
+    $file = file_get_contents($path);
+    $file = explode("\nend\n", $file);
+    $file = array_reverse($file);
+    $return = "";
+    $i = 0;
+    
+    foreach($file as $item)
     {
-	$res['name'] = $res['name'] ? $res['name'] : "ناشناس";
-	echo "• {$res['name']}\n";
+	if($n == $i) break;
+	$item = trim($item);
+	if(!$item) continue;
+	$return .= "- $item\n";
+	$i++;
     }
+    return trim($return);
 }
-else
-{
-    echo "•\n";
-}
-mysqli_close($conn);
-
-/* about.php */
-echo "\n*about.php*\n";
-echo filter_var(file_get_contents("https://allekok.com/about/about-comments.php?num=1"),
-		FILTER_SANITIZE_STRING) . "\n";
-
-/* allekok-desktop */
-echo "\n*allekok-desktop*\n";
-echo @file_get_contents(ABSPATH . "desktop/QA.txt") . "\n";
-
-/* allekok-mobile */
-echo "\n*allekok-mobile*\n";
-echo @file_get_contents(ABSPATH . "mobile/QA.txt") . "\n";
-
-/* pitew */
-echo "\n*پتەوکردنی ئاڵەکۆک*\n";
-echo @file_get_contents(ABSPATH . "pitew/QA.txt") . "\n";
-
-/* dev/tools */
-echo "\n*dev*\n";
-echo @file_get_contents(ABSPATH . "dev/tools/QA.txt") . "\n";
-
-/* manual */
-echo "\n*manual*\n";
-echo @file_get_contents(ABSPATH . "manual/QA.txt") . "\n";
-
-/* CONTRIBUTING */
-echo "\n*CONTRIBUTING*\n";
-echo @file_get_contents(ABSPATH . "dev/tools/CONTRIBUTING/QA.txt") . "\n";
 ?>
