c2f0a4b11df659dd4dc975d80498351e28944e9f
allekok <one@allekok.com>
2019-07-06 04:00:53 +0430

    (pitew/pdfs.php) Add forgotten 'color-555' classes.

--- pitew/pdfs.php
+++ pitew/pdfs.php
@@ -86,7 +86,7 @@ for($i = 0; $i<count($list); $i++) {
     if($name === "فەقێ تەیران - دیوان") {
         echo "<div class='eee'><span class='color-555'>$num.</span> <a href='https://archive.org/download/sarabia_20160323/%D8%AF%DB%8C%D9%88%D8%A7%D9%86%DB%8C%20%D9%81%DB%95%D9%82%DB%8E%20%D8%AA%DB%95%DB%8C%D8%B1%D8%A7%D9%86.pdf'>$name</a> <i class='eee-nfo color-555'>({$list[$i][1]} ,PDF)</i></div>";
     } elseif($name == "سافی هیرانی - دیوان ۲") {
-        echo "<div class='eee'><span>$num.</span> <a href='https://archive.org/download/safi_hirani_diwan/%D8%B3%D8%A7%D9%81%DB%8C%20%D9%87%DB%8C%D8%B1%D8%A7%D9%86%DB%8C%20-%20%D8%AF%DB%8C%D9%88%D8%A7%D9%86.pdf'>$name</a> <i class='eee-nfo'>({$list[$i][1]} ,PDF)</i>";
+        echo "<div class='eee'><span class='color-555'>$num.</span> <a href='https://archive.org/download/safi_hirani_diwan/%D8%B3%D8%A7%D9%81%DB%8C%20%D9%87%DB%8C%D8%B1%D8%A7%D9%86%DB%8C%20-%20%D8%AF%DB%8C%D9%88%D8%A7%D9%86.pdf'>$name</a> <i class='eee-nfo color-555'>({$list[$i][1]} ,PDF)</i>";
         echo "<i class='material-icons' onclick='roll(this)'>info_outline</i>";
         $list[$i][2] = str_replace("\n", "<br>", $list[$i][2]);
         echo "<div class='eee-desc color-555'>{$list[$i][2]}</div></div>";
