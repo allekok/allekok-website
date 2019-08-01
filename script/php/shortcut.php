b64f87fcb0173c9950a719447a39fb16c3eeb3a3
allekok <one@allekok.com>
2019-08-02 00:50:05 +0430

    (script/php/shortcut.php) Add link to "Sware's book"

--- script/php/shortcut.php
+++ script/php/shortcut.php
@@ -17,6 +17,7 @@ $q = isset($_REQUEST['q']) ?
 $assoc = [
     "کوردی" => "https://github.com/allekok/diwan/raw/master/%DA%A9%D9%88%D8%B1%D8%AF%DB%8C%20-%20%D8%AF%DB%8C%D9%88%D8%A7%D9%86%D8%8C%20%D8%A8%DB%95%D8%B1%DA%AF%DB%8C%20%DB%8C%DB%95%DA%A9%DB%95%D9%85.pdf",
     "kurdi" => "https://github.com/allekok/diwan/raw/master/%DA%A9%D9%88%D8%B1%D8%AF%DB%8C%20-%20%D8%AF%DB%8C%D9%88%D8%A7%D9%86%D8%8C%20%D8%A8%DB%95%D8%B1%DA%AF%DB%8C%20%DB%8C%DB%95%DA%A9%DB%95%D9%85.pdf",
+    "sware" => "https://github.com/allekok/diwan/raw/master/%D8%B3%D9%88%D8%A7%D8%B1%DB%95%20%D8%A6%DB%8C%D9%84%D8%AE%D8%A7%D9%86%DB%8C%20%D8%B2%D8%A7%D8%AF%DB%95%20-%20%D8%AA%D8%A7%D9%BE%DB%86%20%D9%88%20%D8%A8%D9%88%D9%88%D9%85%DB%95%D9%84%DB%8E%DA%B5-%20.pdf"
 ];
 
 if(@$assoc[$q])
