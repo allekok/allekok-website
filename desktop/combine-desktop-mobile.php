dc9a326c03f9ef5d23f1bdb32c0f9219b5491da4
allekok <one@allekok.com>
2019-08-20 02:39:18 +0430

    (about/update-comments-txt.php , desktop/combine-desktop-mobile.php) Removed

--- desktop/combine-desktop-mobile.php
+++
@@ -1,7 +0,0 @@
-<?php
-$desktop = 'QA.txt';
-$mobile = '../mobile/QA.txt';
-$new_desktop = file_get_contents($mobile).
-	       file_get_contents($desktop);
-file_put_contents($desktop, $new_desktop);
-?>
