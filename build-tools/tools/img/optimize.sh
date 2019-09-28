6b4b5048d2d173a5ed0896a0089ebf0c094cd580
allekok <one@allekok.com>
2019-09-28 13:15:01 +0330

    (style/img/poets/profile/profile_0.jpg) Compressed.

--- build-tools/tools/img/optimize.sh
+++ build-tools/tools/img/optimize.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 PATH=~/projects/www.allekok.com/style/img/poets/profile
 cd $PATH;
 ls | grep ".jpg" |
     while read f; do
 	jpegtran -progressive $f > $f.0;
 	jpegtran -optimize $f.0 > $f.1;
+	rm $f;
+	mv $f.1 $f
     done
