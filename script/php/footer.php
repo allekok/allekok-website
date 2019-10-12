d8c1de8c0521cc23c57b56debc747ea7182148f0
allekok <one@allekok.com>
2019-10-12 16:37:46 +0330

    (script/js/main-min.js) Minify ’main.js’

--- script/php/footer.php
+++ script/php/footer.php
@@ -11,7 +11,7 @@ if(!$no_foot) {
 	   >arrow_upward</i></a>
     </footer>
     <div class="loader-round" id="main-loader"></div>
-    <script defer src='/script/js/main.js?v48'></script>
+    <script defer src='/script/js/main-min.js?v1'></script>
     <script>	 
      /* Users can evaluate their own code. */
      const userCodes = localStorage.getItem('user-codes') || false;
