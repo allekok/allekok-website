fe33f7684ce98e350c0300fe8cf0483bf92b6647
Payam Bapiri <payambapiri.97@gmail.com>
2020-02-09 19:43:09 +0330

    favicon/make.php: Added, echo the right path prepended by _R

---
+++ favicon/make.php
@@ -0,0 +1,13 @@
+<?php
+require("../script/php/constants.php");
+$inputs = ["browserconfig-sample.xml",
+	   "site-sample.webmanifest"];
+foreach($inputs as $f)
+{
+    file_put_contents(
+	str_replace("-sample", "", $f),
+	str_replace("/favicon/", _R."favicon/",
+		    file_get_contents($f))
+    );
+}
+?>
