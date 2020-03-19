5ff02d9f3b63b4a0df6830a970a3348b6b7073ea
Payam Bapiri <payambapiri.97@gmail.com>
2020-03-19 19:50:03 +0330

    customize/fonts: Some font names corrected.

--- customize/fonts/list.php
+++ customize/fonts/list.php
@@ -1,22 +1,26 @@
 <?php
 const _input = "fonts.txt";
 const _output = "list.txt";
-$fonts = json_decode(file_get_contents(_input), true);
+$fonts = array_diff(scandir("font-imgs"), [".", ".."]);
 $list = [];
-foreach($fonts as $k=>$o) {
+foreach($fonts as $o) {
+    $title = substr($o, 0, -4);
     /* New Name */
-    $s = trim(readline("{$o["title"]}> "));
-    if(!$s) continue;
+    $s = trim(readline("{$title}> "));
     /* change font name */
     $fmt = "";
-    if(file_exists("font-files/{$o["title"]}.ttf"))
+    if(file_exists("font-files/{$title}.ttf"))
 	$fmt = "ttf";
-    elseif(file_exists("font-files/{$o["title"]}.otf"))
+    elseif(file_exists("font-files/{$title}.otf"))
 	$fmt = "otf";
     else continue;
-    exec("mv 'font-files/{$o["title"]}.{$fmt}' 'font-files/{$s}.{$fmt}'");
+    if(!$s) {
+	$list[] = [$title, "png", $fmt];
+	continue;
+    }
+    exec("mv 'font-files/{$title}.{$fmt}' 'font-files/{$s}.{$fmt}'");
     /* change image name */
-    exec("mv 'font-imgs/{$o["title"]}.png' 'font-imgs/{$s}.png'");
+    exec("mv 'font-imgs/{$title}.png' 'font-imgs/{$s}.png'");
     /* list font name, img type, font type */
     $list[] = [$s, "png", $fmt];
 }
