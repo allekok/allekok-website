b07f298a4e653c0ddb250b2e4b414371c216f54d
Payam Bapiri <payambapiri.97@gmail.com>
2020-03-22 04:17:42 +0430

    style: A minor tweak

--- script/php/header.php
+++ script/php/header.php
@@ -21,7 +21,7 @@ if(!$no_head) {
 				    echo $_theme_dark ? 
 					 'main-dark-comp.css' :
 					 'main-comp.css';
-				    ?>?v49'/>
+				    ?>?v50'/>
 	    <title>
 		<?php echo $title; ?>
 	    </title>
