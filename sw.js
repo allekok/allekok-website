ae6d54a25b9988abe110a4bf00d456d15cf23fd0
Payam Bapiri <payambapiri.97@gmail.com>
2020-04-04 20:30:02 +0430

    Styling: Details

--- sw.js
+++ sw.js
@@ -78,7 +78,7 @@ self.addEventListener('install', function(event) {
 		profile+'94.jpg',
 		profile+'96.jpg',
 		'script/js/main-comp.js?v96',
-		'style/css/main-comp.css?v65',
+		'style/css/main-comp.css?v70',
 		'favicon/favicon.ico',
 		'style/font/DroidNaskh-Regular.woff2',
 		'style/font/Material-Icons.woff2',
