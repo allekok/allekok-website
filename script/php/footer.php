38f13bac1ca8e253604680dc2a59fee726d7f16d
Payam Bapiri <payambapiri.97@gmail.com>
2020-04-21 02:31:36 +0430

    main.js: [Left from last commit] version update

--- script/php/footer.php
+++ script/php/footer.php
@@ -12,7 +12,7 @@ if(!$no_foot) {
 	   >arrow_upward</i></button>
     </footer>
     <div class="loader-round" id="main-loader"></div>
-    <script defer src='<?php echo _R; ?>script/js/main-comp.js?v263'></script>
+    <script defer src='<?php echo _R; ?>script/js/main-comp.js?v264'></script>
     <script>
      /* Colors */
      let colors = [<?php
