70f9ed4ebedfd73395d619b7e210d708e9c86f3b
Payam <payambapiri.97@gmail.com>
2020-05-19 11:54:53 +0430

    LICENSE.POEMS: Updated

--- dev/tools/license.php
+++ dev/tools/license.php
@@ -11,17 +11,17 @@ $t_desc = "";
 include(ABSPATH . 'script/php/header.php');
 ?>
 <div id="poets">
-    <h1 style="font-size:1em;text-align:right" class="color-blue">
-	مافی کۆپی‌کردن
-    </h1>
-    <div style="text-align:right;font-size:.6em;
-		padding:1em 2em 0 0">
-	<?php
-	echo str_replace(
-	    "\n", "<br>", file_get_contents(
-		ABSPATH . "LICENSE.POEMS"));
-	?>
-    </div>
+	<h1 style="font-size:1em;text-align:right" class="color-blue">
+		مافی کۆپی‌کردن
+	</h1>
+	<div style="text-align:right;font-size:.6em;
+		    padding:1em 2em 0 0">
+		<?php
+		echo str_replace(
+			".\n", ".<br>\n", file_get_contents(
+				ABSPATH . "LICENSE.POEMS"));
+		?>
+	</div>
 </div>
 <?php
 include_once(ABSPATH . "script/php/footer.php");
