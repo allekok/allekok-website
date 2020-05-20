871513590c2eec3f834c25be48ce015cbfebbb77
Payam <payambapiri.97@gmail.com>
2020-05-20 22:38:34 +0430

    donate/donations: 1st donation(yoohoo)!!!

--- donate/index.php
+++ donate/index.php
@@ -11,82 +11,111 @@ $t_desc = "";
 include(ABSPATH . "script/php/header.php");
 ?>
 <style>
- .donate-main p
- {
-     font-size:.6em
+ .donate-main p {
+	 font-size:.6em
+ }
+ #donation {
+	 display:flex;
+	 font-size:.6em;
+	 border-bottom:1px solid;
+ }
+ #donation div {
+	 width:100%;
+	 padding:0 .5em;
+ }
+ #donation .material-icons {
+	 padding-left:.2em;
  }
 </style>
 <div id="poets" class="donate-main" style="text-align:right">
-    <h1 class="color-blue" style="font-size:1em">
-        یارمەتی‌دانی ماڵی ئاڵەکۆک
-    </h1>
-    <div style="padding-right:1em;padding-bottom:2em">
-	<p>
-	    سڵاو بۆ هاوڕێیانی بەڕێزی ئاڵەکۆک، 
-	</p>
-	<p>
-	    هەر جۆرەی کە ئاگادارن ئاڵەکۆک بەرنامەیەکە کە لە بەکارهێنەرانی هیچ پارەیەکی وەرناگرێ و هیچ پارەیەکیش عاییدی بەکارهێنەرانی نابێ و بەکارهێنەرانی تەنیا بە ویستی خۆیان ئەم بەرنامەی ڕادەگرن و ئەگەر بکرێ پتەوی دەکەن.
-	    <br>
-	    بەڵام ڕاگرتنی ماڵپەڕی ئاڵەکۆک لەسەر هێڵی ئینتێڕنێت پارەی پێویستە هەر بۆیە
-	    تکایە هەرچەندێکی لەتواناتان دایە یارمەتی‌مان بدەن.
-	    پێشەکی زۆرسپاسی یارمەتیەکان‌و دەکەم.
-	</p>
-	<h3 class="color-blue" style="font-size:.7em;padding-top:1em">
-	    ناردنی پارە لەسەر ئینتێرنێت
-	</h3>
-	<p style="padding-right:1em">
-	    بۆ ئەم شێوە لە یارمەتی دەرگای ئەمنی (زرین‌پال)مان بەکارهێناوە. تکایە لەسەر لینکی خوارەوە کرتە بکەن. پێشەکی سپاس‌و دەکەین.
-	    <br>
-	    <a href="https://zarinp.al/@allekok"
-	       title="یارمەتی‌دانی ماڵی ئاڵەکۆک"
-	       class="link-underline" style="display:inline-block;font-weight:bold;text-align:center"
-	       target="_blank">
-		ناردنی پارە بەشێوەی ئینتێرنێتی
-	    </a>
-	</p>
-	<h3 class="color-blue" style="font-size:.7em;padding-top:1em">
-	    ناردنی پارە بەشێوەی ڕاستەوخۆ (کارت بە کارت)
-	</h3>
-	<p style="padding-right:1em">
-	    تکایە یارمەتی‌تان بۆ ژمارە کارتی خوارەوە بنێرن. پێشەکی سپاس‌و دەکەین.
-	    <br>
-	    <span style="padding: .5em 1em;
-			 display: block;
-			 text-align: center;
-			 border:2px solid;
-			 border-radius:1em;
-			 margin:1em 0">
-		<span style="letter-spacing: .2em;font-size: 1.3em">
-		    ٦٠٣٧٩٩٧١٣٥٣٩٤٥٨٤
-		</span>
-		<br>
-		کارتی بانکی ملی
-		<br>
-		بە نێوی: پیام باپیری
-	    </span>
-	    تکایە ئەگەر بەشێوەی ڕاستەوخۆ (کارت بە کارت) یارمەتی‌مان دەدەن، نێوی خۆتان و ئەندازەی یارمەتیەکەتان‌ بە ئیمەیل یان لەسەر تێلێگرام بۆمان بنێرن.
-	    <br>
-	    ئیمەیلی ئاڵەکۆک:
-	    <i style="font-weight:bold;
-		      direction:ltr;display:inline-block;
-		      font-family:monospace;padding:0 .5em"
-	    >one@allekok.ir</i>
-	    <br>
-	    ئایدی تێلێگرامی ئاڵەکۆک:
-	    <i style="font-weight:bold;
-		      direction:ltr;display:inline-block;
-		      font-family:monospace;padding:0 .5em"
-	    >@allekok</i>
-	</p>
-	<p style="padding-right:1em">
-	</p>
-	<h2 class="color-blue" style="font-size:1em">
-	    یارمەتیدەران
-	</h2>
-	<p style="padding-right:1em">
-	    تا ئێستا هیچ کەس یارمەتی نەناردووە. ئێوە یەکەم کەس بن.
-	</p>
-    </div>
+	<h1 class="color-blue" style="font-size:1em">
+		یارمەتی‌دانی ماڵی ئاڵەکۆک
+	</h1>
+	<div style="padding-right:1em;padding-bottom:2em">
+		<p>
+			سڵاو بۆ هاوڕێیانی بەڕێزی ئاڵەکۆک، 
+		</p>
+		<p>
+			هەر جۆرەی کە ئاگادارن ئاڵەکۆک بەرنامەیەکە کە لە بەکارهێنەرانی هیچ پارەیەکی وەرناگرێ و هیچ پارەیەکیش عاییدی بەکارهێنەرانی نابێ و بەکارهێنەرانی تەنیا بە ویستی خۆیان ئەم بەرنامەی ڕادەگرن و ئەگەر بکرێ پتەوی دەکەن.
+			<br>
+			بەڵام ڕاگرتنی ماڵپەڕی ئاڵەکۆک لەسەر هێڵی ئینتێڕنێت پارەی پێویستە هەر بۆیە
+			تکایە هەرچەندێکی لەتواناتان دایە یارمەتی‌مان بدەن.
+			پێشەکی زۆرسپاسی یارمەتیەکان‌و دەکەم.
+		</p>
+		<h3 class="color-blue" style="font-size:.7em;padding-top:1em">
+			ناردنی پارە لەسەر ئینتێرنێت
+		</h3>
+		<p style="padding-right:1em">
+			بۆ ئەم شێوە لە یارمەتی دەرگای ئەمنی (زرین‌پال)مان بەکارهێناوە. تکایە لەسەر لینکی خوارەوە کرتە بکەن. پێشەکی سپاس‌و دەکەین.
+			<br>
+			<a href="https://zarinp.al/@allekok"
+			   title="یارمەتی‌دانی ماڵی ئاڵەکۆک"
+			   class="link-underline" style="display:inline-block;font-weight:bold;text-align:center"
+			   target="_blank">
+				ناردنی پارە بەشێوەی ئینتێرنێتی
+			</a>
+		</p>
+		<h3 class="color-blue" style="font-size:.7em;padding-top:1em">
+			ناردنی پارە بەشێوەی ڕاستەوخۆ (کارت بە کارت)
+		</h3>
+		<p style="padding-right:1em">
+			تکایە یارمەتی‌تان بۆ ژمارە کارتی خوارەوە بنێرن. پێشەکی سپاس‌و دەکەین.
+			<br>
+			<span style="padding: .5em 1em;
+				     display: block;
+				     text-align: center;
+				     border:2px solid;
+				     border-radius:1em;
+				     margin:1em 0">
+				<span style="letter-spacing: .2em;font-size: 1.3em">
+					٦٠٣٧٩٩٧١٣٥٣٩٤٥٨٤
+				</span>
+				<br>
+				کارتی بانکی ملی
+				<br>
+				بە نێوی: پیام باپیری
+			</span>
+			تکایە ئەگەر بەشێوەی ڕاستەوخۆ (کارت بە کارت) یارمەتی‌مان دەدەن، نێوی خۆتان و ئەندازەی یارمەتیەکەتان‌ بە ئیمەیل یان لەسەر تێلێگرام بۆمان بنێرن.
+			<br>
+			ئیمەیلی ئاڵەکۆک:
+			<i style="font-weight:bold;
+				  direction:ltr;display:inline-block;
+				  font-family:monospace;padding:0 .5em"
+			>one@allekok.ir</i>
+			<br>
+			ئایدی تێلێگرامی ئاڵەکۆک:
+			<i style="font-weight:bold;
+				  direction:ltr;display:inline-block;
+				  font-family:monospace;padding:0 .5em"
+			>@allekok</i>
+		</p>
+		<p style="padding-right:1em">
+		</p>
+		<h2 class="color-blue" style="font-size:1em">
+			یارمەتیدەران
+		</h2>
+		<div style="padding-right:1em">
+			<?php
+			$donations = @file_get_contents("donations");
+			if($donations) {
+				echo "<p>زۆرسپاس بۆ یارمەتیەکانتان:</p>";
+				$donations = explode("-----", $donations);
+				foreach($donations as $d) {
+					if(!($d = trim($d))) continue;
+					$d = explode("\t", $d);
+					echo "
+<div id='donation'>
+<div><i class='material-icons color-blue'>favorite</i>$d[0]</div>
+<div>$d[1]</div><div>$d[2]</div><div>$d[3]</div>
+</div>";
+				}
+			}
+			else 
+				echo "<p>
+تا ئێستا هیچ کەس یارمەتی نەناردووە. ئێوە یەکەم کەس بن.</p>";
+			?>
+		</div>
+	</div>
 </div>
 <?php
 include_once(ABSPATH . "script/php/footer.php");
