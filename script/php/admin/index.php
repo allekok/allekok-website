24856eb20efc527bc4de1dbaf8db1e6470eef43d
Payam <payambapiri.97@gmail.com>
2020-05-21 10:16:06 +0430

    Minor tweaks

--- script/php/admin/index.php
+++ script/php/admin/index.php
@@ -13,34 +13,35 @@ include(ABSPATH . 'script/php/header.php');
 ?>
 <style>
  .line {
-     direction:ltr;
-     text-align:left;
-     font-family:'kurd',monospace;
-     font-size:.8em;
+	 direction:ltr;
+	 text-align:left;
+	 font-family:'kurd',monospace;
+	 font-size:.8em;
  }
  .line a {
-     padding:.5em;
-     display:block;
+	 padding:.5em;
+	 display:block;
  }
 </style>
 <div id="poets">
-    <?php    
-    $files = scandir("./");
-    rsort($files);
-    $NOT = [
-	".","..","IP-blacklist.php","IP-blacklist-sample.php",
-	"SHA512.php","capture","comment-block.php","comment-read.php",
-	"index.php","link-ganjoor.php","login.php","password.php",
-	"password-sample.php","session.php","error_log",".htaccess",
-	"sql-columns.php","sanitize-poems.php","get-pitew.php",
-    ];
-    foreach($files as $f)
-    {
-	if(!in_array($f, $NOT))
-	    echo "<p class='line'><a class='link' href='$f'>&rsaquo; ".
-		 substr($f,0,-4)."</a></p>";
-    }
-    ?>
+	<?php    
+	$files = scandir("./");
+	rsort($files);
+	$NOT = [
+		".","..","IP-blacklist.php","IP-blacklist-sample.php",
+		"SHA512.php","capture","comment-block.php","comment-read.php",
+		"index.php","link-ganjoor.php","login.php","password.php",
+		"password-sample.php","session.php","error_log",".htaccess",
+		"sql-columns.php","sanitize-poems.php","get-pitew.php",
+		"set-pitew.php",
+	];
+	foreach($files as $f)
+	{
+		if(!in_array($f, $NOT))
+			echo "<p class='line'><a class='link' href='$f'>&rsaquo; ".
+			     substr($f,0,-4)."</a></p>";
+	}
+	?>
 </div>
 <?php
 include_once(ABSPATH . "script/php/footer.php");
