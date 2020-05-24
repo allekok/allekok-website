4ac581d17f46ef0f112fba16b4aec3f23f37345f
Payam <payambapiri.97@gmail.com>
2020-05-24 21:38:45 +0430

    script/php/admin/new-book-table.php: Make 'id' column, the primary column.

--- script/php/admin/new-book-table.php
+++ script/php/admin/new-book-table.php
@@ -2,20 +2,20 @@
 require('session.php');
 
 header("Content-type: text/plain; charset=UTF-8");
-echo "GET pt_id, bk_id\n";
+echo "GET pt, bk\n";
 
-$pt_id = @filter_var($_GET['pt_id'], FILTER_VALIDATE_INT) ?
-	 $_GET['pt_id'] : die("`pt_id` is not a valid number.\n");
-$bk_id = @filter_var($_GET['bk_id'], FILTER_VALIDATE_INT) ?
-	 $_GET['bk_id'] : die("`bk_id` is not a valid number.\n");
+$pt = @filter_var($_GET['pt'], FILTER_VALIDATE_INT) ?
+      $_GET['pt'] : die("`pt` is not a valid number.\n");
+$bk = @filter_var($_GET['bk'], FILTER_VALIDATE_INT) ?
+      $_GET['bk'] : die("`bk` is not a valid number.\n");
 
 
-$tbl_name = "tbl{$pt_id}_$bk_id";
-$q = "CREATE TABLE IF NOT EXISTS `{$tbl_name}` (`id` INT(16), `name` TEXT, `hon` TEXT, `hdesc` TEXT, `link` TEXT, `lang` TEXT, `tag` TEXT) ENGINE = MyISAM CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci";
+$tbl_name = "tbl{$pt}_$bk";
+$q = "CREATE TABLE IF NOT EXISTS `{$tbl_name}` (`id` INT(16) PRIMARY KEY, `name` TEXT, `hon` TEXT, `hdesc` TEXT, `link` TEXT, `lang` TEXT, `tag` TEXT) ENGINE = MyISAM CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci";
 require("../condb.php");
 
 if($query)
-    echo "Table `{$tbl_name}` created successfully.\n";
+	echo "Table `{$tbl_name}` created successfully.\n";
 else
-    echo "Table `{$tbl_name}` creating failed.\n";
+	echo "Table `{$tbl_name}` creating failed.\n";
 ?>
