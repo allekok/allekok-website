249448c8ebdd51da859015d497b6576729ff2c28
Payam <payambapiri.97@gmail.com>
2020-08-10 18:54:03 +0430

    profile_27.jpg: Added

--- sw.js
+++ sw.js
@@ -81,6 +81,7 @@ self.addEventListener('install', function(event) {
 				profile+'70.jpg',
 				profile+'66.jpg',
 				profile+'98.jpg',
+				profile+'27.jpg',
 				'script/js/main-comp.js?v447',
 				'style/css/main-comp.css?v121',
 				'favicon/favicon.ico',
