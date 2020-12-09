b71496136a86d253126d698af77a2672658e3dee
Payam <payambapiri.97@gmail.com>
2020-12-09 15:04:18 +0330

    pitew/poetdesc-list.php: UI improvements

--- pitew/poetdesc-list.php
+++ pitew/poetdesc-list.php
@@ -20,17 +20,16 @@ include(ABSPATH . 'script/php/header.php');
 ?>
 <style>
  #poetdesc-list-main .epld {
-	 padding: 1em;
 	 font-size: .6em;
 	 text-align: right;
  }
  #poetdesc-list-main .epld-title {
-	 padding: 0 1em;
+	 padding: 0 .5em 0 0;
 	 border-right: 2px solid;
 	 font-size: 1.05em;
  }
  #poetdesc-list-main .epld-body {
-	 padding:1em;
+	 padding:1em 1em 1em 0;
 	 text-align:justify;
 	 word-wrap:break-word;
  }
