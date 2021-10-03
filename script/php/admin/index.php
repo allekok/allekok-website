ab310def96e0e102968ab7f755c76c3de9839de4
Payam <payambapiri.97@gmail.com>
2021-10-04 02:47:28 +0330

    script/php/admin/index.php: Clean-up code.

--- script/php/admin/index.php
+++ script/php/admin/index.php
@@ -1,47 +1,69 @@
 <?php
-require('session.php');
-include_once("../constants.php");
-include_once(ABSPATH . "script/php/colors.php");
-include_once(ABSPATH . "script/php/functions.php");
+require_once("session.php");
+require_once("../constants.php");
+require_once("../colors.php");
+require_once("../functions.php");
 
-$title = $_TITLE . " &rsaquo; مدیر";
+$title = $_TITLE . " › مدیر";
 $desc = "مدیر";
 $keys = $_KEYS;
 $t_desc = "";
 
-include(ABSPATH . 'script/php/header.php');
+require_once("../header.php");
 ?>
 <style>
  .line {
 	 text-align:right;
-	 font-family:'kurd',monospace;
-	 font-size:.7em;
+	 font-family:"kurd", monospace;
+	 font-size:1.15em;
+	 padding-right:1em;
  }
  .line a {
 	 padding:.3em;
 	 display:block;
  }
 </style>
-<div id="poets">
-	<?php    
-	$files = scandir("./");
+<div id="poets" style="font-size:.55em;text-align:right">
+	<h1 style="font-size:2em" class="color-blue">
+		مدیر
+	</h1>
+	<?php
+	$files = scandir(".");
 	rsort($files);
-	$NOT = [
-		".","..","IP-blacklist.php","IP-blacklist-sample.php",
-		"SHA512.php","capture","comment-block.php","comment-read.php",
-		"index.php","link-ganjoor.php","login.php","password.php",
-		"password-sample.php","session.php","error_log",".htaccess",
-		"sql-columns.php","get-pitew.php","set-pitew.php",
-		"normalize.php", "install.php",
+	$ignore = [
+		".",
+		"..",
+		"IP-blacklist.php",
+		"IP-blacklist-sample.php",
+		"SHA512.php",
+		"capture",
+		"comment-block.php",
+		"comment-read.php",
+		"index.php",
+		"link-ganjoor.php",
+		"login.php",
+		"password.php",
+		"password-sample.php",
+		"session.php",
+		"error_log",
+		".htaccess",
+		"sql-columns.php",
+		"get-pitew.php",
+		"set-pitew.php",
+		"normalize.php",
+		"install.php",
 	];
-	foreach($files as $f)
-	{
-		if(!in_array($f, $NOT))
-			echo "<p class='line'><a class='link' href='$f'>&rsaquo; ".
-			     substr($f,0,-4)."</a></p>";
+	foreach($files as $f) {
+		if(!in_array($f, $ignore)) {
+			echo "<p class='line'>" .
+			     "<a class='link' href='$f'>" .
+			     "› " .
+			     substr($f, 0, -4) .
+			     "</a></p>";
+		}
 	}
 	?>
 </div>
 <?php
-include_once(ABSPATH . "script/php/footer.php");
+require_once("../footer.php");
 ?>
