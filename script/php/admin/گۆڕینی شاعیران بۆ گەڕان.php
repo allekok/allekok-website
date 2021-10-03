b900bf468bf694a1cb8ea62eb35b642aee5283de
Payam <payambapiri.97@gmail.com>
2021-10-04 03:05:14 +0330

    script/php/admin/گۆڕینی شاعیران بۆ گەڕان.php: Clean-up code.

--- script/php/admin/گۆڕینی شاعیران بۆ گەڕان.php
+++ script/php/admin/گۆڕینی شاعیران بۆ گەڕان.php
@@ -1,78 +1,91 @@
 <?php
-require('session.php');
-include_once("../constants.php");
-include_once(ABSPATH . "script/php/colors.php");
-include_once(ABSPATH . "script/php/functions.php");
+require_once("session.php");
+require_once("../constants.php");
+require_once("../colors.php");
+require_once("../functions.php");
 
-$title = $_TITLE . " &rsaquo; شاعیران";
-$desc = "شاعیران";
+$title = $_TITLE . " › گۆڕینی شاعیران بۆ گەڕان";
+$desc = "گۆڕینی شاعیران بۆ گەڕان";
 $keys = $_KEYS;
 $t_desc = "";
 
-include(ABSPATH . 'script/php/header.php');
+require_once("../header.php");
 ?>
 <style>
  .line {
 	 text-align:right;
-	 font-size:.65em;
+	 font-size:1.1em;
 	 padding:0 1em;
  }
 </style>
-<div id="poets">
+<div id="poets" style="font-size:.55em;text-align:right">
+	<h1 style="font-size:2em" class="color-blue">
+		گۆڕینی شاعیران بۆ گەڕان
+	</h1>
 	<?php
-	/* READ */
-	$q = "SELECT * FROM `auth` ORDER BY `takh` ASC";
-	require(ABSPATH."script/php/condb.php");
+	/* Read */
+	$q = "SELECT * FROM auth ORDER BY takh ASC";
+	require_once("../condb.php");
+	
 	$aths_num = mysqli_num_rows($query);
 	$aths = [];
-
-	while($res=mysqli_fetch_assoc($query))
-	{
-		$res['rtakh'] = $res['takh'];
-		$res['name'] = san_data($res['name']);
-		$res['takh'] = san_data($res['takh']);
-		$res['profname'] = san_data($res['profname']);
-		$res['hdesc'] = san_data($res['hdesc']);
-		$res['len'] = (strlen($res['name']) > strlen($res['hdesc'])) ?
-			      strlen($res['name']) : strlen($res['hdesc']);
+	
+	while($res = mysqli_fetch_assoc($query)) {
+		$res["rtakh"] = $res["takh"];
+		$res["name"] = san_data($res["name"]);
+		$res["takh"] = san_data($res["takh"]);
+		$res["profname"] = san_data($res["profname"]);
+		$res["hdesc"] = san_data($res["hdesc"]);
+		if(strlen($res["name"]) > strlen($res["hdesc"]))
+			$res["len"] = strlen($res["name"]);
+		else
+			$res["len"] = strlen($res["hdesc"]);
 		$aths[] = $res;
 	}
-
-	/* WRITE */
-	$string = "<p class='line'>ئەژماری شاعیران: ".
-		  num_convert($aths_num,'en','ckb')."</p>";
+	
+	/* Write */
+	$string = "<p class='line'>ئەژماری شاعیران: " .
+		  num_convert($aths_num, "en", "ckb") .
+		  "</p>";
 	$error = false;
-	mysqli_select_db($conn,_SEARCH_DB);
-	mysqli_query($conn,"TRUNCATE TABLE poets");
-	foreach($aths as $ath)
-	{
-		$id = $ath['id'];
-		$name = $ath['name'];
-		$takh = $ath['takh'];
-		$profname = $ath['profname'];
-		$hdesc = $ath['hdesc'];
-		$rtakh = $ath['rtakh'];
-		$len = $ath['len'];
+	mysqli_select_db($conn, _SEARCH_DB);
+	mysqli_query($conn, "TRUNCATE TABLE poets");
+	
+	foreach($aths as $ath) {
+		$id = $ath["id"];
+		$name = $ath["name"];
+		$takh = $ath["takh"];
+		$profname = $ath["profname"];
+		$hdesc = $ath["hdesc"];
+		$rtakh = $ath["rtakh"];
+		$len = $ath["len"];
 		
-		$query = mysqli_query($conn,
-				      "INSERT INTO `poets`(`id`,`name`,`takh`,
-`profname`,`hdesc`,`rtakh`,`len`) VALUES($id,'$name','$takh',
-'$profname','$hdesc','$rtakh','$len')");
+		$query = mysqli_query(
+			$conn,
+			"INSERT INTO `poets`(`id`, `name`, `takh`," .
+			" `profname`, `hdesc`, `rtakh`, `len`)" .
+			" VALUES($id, '$name', '$takh'," .
+			" '$profname', '$hdesc', '$rtakh', '$len')");
 		
-		if(!$query)
-		{
+		if(!$query) {
 			$error = true;
-			$string.="<p class='line'><a class='link' href='/poet:$id'
->$rtakh</a>: <b style='color:red'>نەء!</b></p>";
+			$string .= "<p class='line'>" .
+				   "<a class='link' " .
+				   "href='/poet:$id'>" .
+				   "$rtakh</a>: <b class=" .
+				   "'color-red'>نەء!</b></p>";
 		}
 	}
+	
 	mysqli_close($conn);
 	echo $string;
-	if(!$error)
-		echo "<p class='line' style='text-align:center'
-><b style='color:green'>جێ‌بە‌جێیە.</b></p>";
+	
+	if(!$error) {
+		echo "<p class='line' style='text-align:center'>" .
+		     "<b class='color-blue'>جێ بە جێیە.</b></p>";
+	}
 	?>
 </div>
 <?php
-include_once(ABSPATH . "script/php/footer.php");
+require_once("../footer.php");
 ?>
