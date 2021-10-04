9ec57ca93527fff129511dc5c946225c8325a6ea
Payam <payambapiri.97@gmail.com>
2021-10-04 04:21:00 +0330

    script/php/admin/comment-{block, read}.php: Clean-up codes.

--- script/php/admin/comment-block.php
+++ script/php/admin/comment-block.php
@@ -1,24 +1,24 @@
 <?php
 require_once("session.php");
-if(filter_var($_GET["id"], FILTER_VALIDATE_INT) !== false) {
-	$id = $_GET["id"];
-	
-	$block = 1;
-	$read = 1;
-	
-	$q = "SELECT blocked FROM comments WHERE id={$id}";
-	require_once("../condb.php");
-	if($query and mysqli_fetch_assoc($query)["blocked"]) {
-		$block = 0;
-		$read = 0;
-	}
 
-	$q = "UPDATE `comments` " .
-	     "SET `blocked`={$block}, `read`={$read} " .
-	     "WHERE `id`={$id}";
-	if($query = mysqli_query($conn, $q))
-		echo 1;
-	
-	mysqli_close($conn);
+$id = filter_var($_GET["id"], FILTER_VALIDATE_INT) !== false ?
+      $_GET["id"] :
+      die();
+$block = 1;
+$read = 1;
+
+$q = "SELECT blocked FROM comments WHERE id={$id}";
+require_once("../condb.php");
+if($query and mysqli_fetch_assoc($query)["blocked"]) {
+	$block = 0;
+	$read = 0;
 }
+
+$q = "UPDATE `comments` " .
+     "SET `blocked`={$block}, `read`={$read} " .
+     "WHERE `id`={$id}";
+if($query = mysqli_query($conn, $q))
+	echo 1;
+
+mysqli_close($conn);
 ?>
--- script/php/admin/comment-read.php
+++ script/php/admin/comment-read.php
@@ -1,19 +1,19 @@
 <?php
 require_once("session.php");
-if(filter_var($_GET['id'], FILTER_VALIDATE_INT) !== false) {
-	$id = $_GET['id'];
-	
-	$read = 1;
-	
-	$q = "SELECT `read` FROM comments WHERE id={$id}";
-	require_once("../condb.php");
-	if($query and mysqli_fetch_assoc($query)["read"])
-		$read = 0;
-	
-	$q = "UPDATE `comments` SET `read`={$read} WHERE `id`={$id}";
-	if($query = mysqli_query($conn, $q))
-		echo 1;
-	
-	mysqli_close($conn);
-}
+
+$id = filter_var($_GET["id"], FILTER_VALIDATE_INT) !== false ?
+      $_GET["id"] :
+      die();
+$read = 1;
+
+$q = "SELECT `read` FROM comments WHERE id={$id}";
+require_once("../condb.php");
+if($query and mysqli_fetch_assoc($query)["read"])
+	$read = 0;
+
+$q = "UPDATE `comments` SET `read`={$read} WHERE `id`={$id}";
+if($query = mysqli_query($conn, $q))
+	echo 1;
+
+mysqli_close($conn);
 ?>
