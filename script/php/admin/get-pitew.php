b6d3fdf2db0d69064e3b06b58f83e8510cc6f5b8
Payam <payambapiri.97@gmail.com>
2021-10-04 04:18:13 +0330

    script/php/admin/get-pitew.php: Clean-up code.

--- script/php/admin/get-pitew.php
+++ script/php/admin/get-pitew.php
@@ -1,26 +1,27 @@
 <?php
-require('session.php');
-require('../constants.php');
+require_once("session.php");
+require_once("../constants.php");
 
-$id = false !== filter_var($_GET['id'], FILTER_VALIDATE_INT) ?
-      $_GET['id'] : die();
+$id = filter_var($_GET["id"], FILTER_VALIDATE_INT) !== false ?
+      $_GET["id"] :
+      die();
 
 $q = "SELECT * FROM pitew WHERE id=$id";
-require(ABSPATH.'script/php/condb.php');
-if(!$query) die();
+require_once("../condb.php");
+if(!$query)
+	die();
 
 $row = mysqli_fetch_assoc($query);
 $statusObj = json_decode($row["status"], true);
 
 header("Content-type:application/json; charset=utf-8");
 echo json_encode([
-	'contributor'=>$row['contributor'],
-	'poet'=>$row['poet'],
-	'book'=>$row['book'],
-	'poemName'=>$row['poem-name'],
-	'poem'=>$row['poem'],
-	'status'=>$statusObj['status'],
-	'adrs'=>$statusObj['url'],
-	'desc'=>$statusObj['desc'],
-]);
+	"contributor" => $row["contributor"],
+	"poet" => $row["poet"],
+	"book" => $row["book"],
+	"poemName" => $row["poem-name"],
+	"poem" => $row["poem"],
+	"status" => $statusObj["status"],
+	"adrs" => $statusObj["url"],
+	"desc" => $statusObj["desc"]]);
 ?>
