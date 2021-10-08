83895f373f2fdbbe884da7782a89de6c0eed9b72
Payam <payambapiri.97@gmail.com>
2021-10-08 17:14:06 +0330

    script/php/admin/session.php: Clean-up code.

--- script/php/admin/session.php
+++ script/php/admin/session.php
@@ -1,39 +1,42 @@
 <?php
-require_once("../constants.php");
-require_once(ABSPATH."script/php/functions.php");
+require_once("../functions.php");
+
 session_start();
-if(! isset($_SESSION['admin'])) {
-	$IP = $_SERVER['REMOTE_ADDR'];
-	if(is_IP_blocked($IP,3)) die();
-	require('password.php');
-	$received_pass = isset($_POST['password']) ?
-			 hash('SHA512',stripslashes($_POST['password'])) :
-			 redirect(_R.'script/php/admin/login.php');
-	if($password == $received_pass) {
-		$_SESSION['admin'] = true;
-	}
-	else {
+
+if(!isset($_SESSION["admin"])) {
+	$IP = $_SERVER["REMOTE_ADDR"];
+	if(is_IP_blocked($IP, 3))
+		die();
+	require_once("password.php");
+	$rec_pass = isset($_POST["password"]) ?
+		    hash("SHA512", stripslashes($_POST["password"])) :
+		    redirect("login.php");
+	if($password == $rec_pass) {
+		$_SESSION["admin"] = true;
+	} else {
 		session_destroy();
 		add_IP_to_blacklist($IP);
+		redirect("login.php");
 		die();
 	}
 }
 
-function is_IP_blocked($IP,$n) {
-	require('IP-blacklist.php');
-	return null !== @$IP_blacklist[$IP] ?
-	       $IP_blacklist[$IP] >= $n : false;
+/* Functions */
+function is_IP_blocked($IP, $n) {
+	require_once("IP-blacklist.php");
+	return isset($IP_blacklist[$IP]) ?
+	       $IP_blacklist[$IP] >= $n :
+	       false;
 }
-
 function add_IP_to_blacklist($IP) {
 	$IP = filter_var($IP, FILTER_SANITIZE_STRING);
-	require('IP-blacklist.php');
+	require("IP-blacklist.php");
 	@$IP_blacklist[$IP] += 1;
-	$string .= "<?php\n\$IP_blacklist=[";
-	foreach($IP_blacklist as $IP=>$n) {
-		$string .= "\n'$IP'=>$n,";
+	$string = "<?php\n\$IP_blacklist = [";
+	foreach($IP_blacklist as $IP => $n) {
+		$string .= "\n\t\"$IP\" => $n,";
 	}
 	$string .= "\n];\n?>";
-	file_put_contents('IP-blacklist.php',$string);
+	file_put_contents("IP-blacklist.php", $string);
 }
 ?>
