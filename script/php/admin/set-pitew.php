5fd0fa823d51360c1862e99a75c77a7dfff79b79
Payam <payambapiri.97@gmail.com>
2021-10-08 17:11:01 +0330

    script/php/admin/set-pitew.php: Clean-up code.

--- script/php/admin/set-pitew.php
+++ script/php/admin/set-pitew.php
@@ -1,22 +1,22 @@
 <?php
-require('session.php');
-require('../constants.php');
+require_once("session.php");
 
-$id = false !== filter_var($_GET['id'], FILTER_VALIDATE_INT) ?
-      $_GET['id'] : die("0");
-$status = false !== filter_var($_GET['status'], FILTER_VALIDATE_INT) ?
-	  intval($_GET['status']) : 0;
-$adrs = @filter_var(trim($_GET['adrs']), FILTER_SANITIZE_STRING);
-$desc = @filter_var(trim($_GET['desc']), FILTER_SANITIZE_STRING);
+$id = filter_var($_GET["id"], FILTER_VALIDATE_INT) !== false ?
+      $_GET["id"] :
+      die("0");
+$status = filter_var($_GET["status"], FILTER_VALIDATE_INT) !== false ?
+	  intval($_GET["status"]) :
+	  0;
+$adrs = @filter_var(trim($_GET["adrs"]), FILTER_SANITIZE_STRING);
+$desc = @filter_var(trim($_GET["desc"]), FILTER_SANITIZE_STRING);
 
 $statusObj = json_encode([
 	"status" => $status,
 	"url" => $adrs,
-	"desc" => $desc,
+	"desc" => $desc
 ]);
 
 $q = "UPDATE pitew SET status='$statusObj' WHERE id=$id";
-require(ABSPATH.'script/php/condb.php');
-if($query) echo "1";
-else echo "0";
+require_once("../condb.php");
+echo $query ? "1" : "0";
 ?>
