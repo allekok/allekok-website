d33d1aea55ea75830375aaa50fdd1b8dd5942d1e
Payam <payambapiri.97@gmail.com>
2022-02-03 16:25:14 +0330

    /about: Trying to cleanup

--- about/about-comments.php
+++ about/about-comments.php
@@ -1,24 +1,32 @@
 <?php
 /* 
- * Reading comments from {uri} file and print them. 
- * Input: GET [num,plain]
+ * Reading comments from {comments_file} file and print them.
+ * Input: GET[num, plain]
  * Output: Text, HTML
  */
 require("functions.php");
 
-if(! file_exists(comments_file)) die();
+if(!file_exists(comments_file))
+	die();
 $comments = file_get_contents(comments_file);
-$comments = explode("[comment]",$comments);
-$comments = array_reverse($comments);
+$comments = explode("[comment]", $comments);
+$last_comment_idx = count($comments) - 1;
 
-$limit = false === filter_var(@$_GET['num'], FILTER_VALIDATE_INT) ?
-	-1 : $_GET['num'];
+$limit = filter_var(@$_GET["num"], FILTER_VALIDATE_INT) === false ?
+	-1 :
+	 $_GET["num"];
+$plain = isset($_GET["plain"]);
 
-foreach($comments as $c)
-{
-	if($limit-- == 0) break;
-	if(!isset($_GET["plain"]))
-		$c = str_replace("\n", "<br>", $c);
-	echo $c;
+if($plain) {
+	header("Content-type: text/plain; Charset=UTF-8");
+	print_comments(function($c) {
+		return $c;
+	});
+}
+else {
+	header("Content-type: text/html; Charset=UTF-8");
+	print_comments(function($c) {
+		return str_replace("\n", "<br>", $c);
+	});
 }
 ?>
--- about/functions.php
+++ about/functions.php
@@ -6,7 +6,7 @@ require_once(ABSPATH . "script/php/kurdish-calendar.php");
 const comments_file = "comments.txt";
 const comment_sign = "[comment]";
 
-function calendar_kurdish_string ($date=NULL) {
+function calendar_kurdish_string($date=NULL) {
 	if(!$date)
 		$date = calendarKurdishFromGregorian(
 			explode("-", date("m-d-Y")));
@@ -14,8 +14,14 @@ function calendar_kurdish_string ($date=NULL) {
 	$month = calendarKurdishMonth(
 		calendarExtractMonth($date));
 	$day = calendarExtractDay($date);
-
+	
 	$str = "{$day}ی {$month}ی {$year}ی کوردی";
 	return num_convert($str, "en", "ckb");
+}
+
+function print_comments($f) {
+	global $comments, $last_comment_idx, $limit;
+	for($i = $last_comment_idx; $i > -1 and $limit; $i--, $limit--)
+		echo $f($comments[$i]);
 }
 ?>
