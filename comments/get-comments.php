6e71a3c996de8e21545967c74f50fe68c98a4f5a
Payam <payambapiri.97@gmail.com>
2022-02-04 11:37:12 +0330

    comments/*: Clean up code

--- comments/get-comments.php
+++ comments/get-comments.php
@@ -1,67 +1,53 @@
 <?php
 /*
- * Print out {n} comments from 'comments' table.
- * Input: GET(n)
+ * Print {n} comments from 'comments' table.
+ * Input: GET[n, from]
  * Output: JSON
  */
-include_once("../script/php/constants.php");
-include_once(ABSPATH . "script/php/functions.php");
+require_once("../script/php/constants.php");
+require_once("../script/php/functions.php");
 
-header("Content-Type: application/json; charset=UTF-8");
+header("Content-Type: application/json; Charset=UTF-8");
 
-/* Number of comments */
-$n = @filter_var($_GET["n"], FILTER_VALIDATE_INT) ?
-     $_GET["n"] : 20;
-$LIMIT = $n == -1 ? "" : "LIMIT 0, $n";
+$n = isset($_GET["n"]) ? intval($_GET["n"]) : 20;
+$from = isset($_GET["from"]) ? intval($_GET["from"]) : 0;
 
-/* Query for non-blocked comments */
-$q = "select * from comments where blocked=0 order by id DESC $LIMIT";
-include(ABSPATH . "script/php/condb.php");
-if(!$query) die(json_encode(["err"=>1]));
+$LIMIT = $n == -1 ? "" : "LIMIT $from, $n";
+$q = "SELECT * FROM comments WHERE blocked=0 ORDER BY id DESC $LIMIT";
+require_once("../script/php/condb.php");
+if(!$query)
+	die(json_encode(["err" => 1]));
 
-$comms = []; // comments
-while($res = mysqli_fetch_assoc($query))
-{
-	/* Poet's id from address(poet:{$pt}/...) */
-	/* 5 = strlen("poet:") */
-	$res["pt"] = substr($res['address'], 5,
-			    strpos($res['address'], "/") - 5);
-	/* Unset unnecessary fields */
+$comms = [];
+
+while($res = mysqli_fetch_assoc($query)) {
+	$res["pt"] = substr($res["address"],
+			    5,
+			    strpos($res["address"], "/") - 5);
 	unset($res["read"]);
 	unset($res["blocked"]);
-	
 	$comms[] = $res;
 }
 
-/* Fetch Poet's name, Poem title for each comment. */
-foreach($comms as $key => $comm)
-{
-	/* Split address by slashes ('/') */
+foreach($comms as $key => $comm) {
 	$_adrs = explode("/", $comm["address"]);
 	$_adrs_len = count($_adrs);
 	for($i = 0; $i < $_adrs_len; $i++)
-	{
-		/* split [$_adrs] elements by ":"
-		   [ [0] => ["poet", "poet's id"], ... ] */
 		$_adrs[$i] = explode(":", $_adrs[$i]);
-	}
-
-	/* Poet's name */
-	$q = "select takh from auth where id={$_adrs[0][1]}";
+	
+	$q = "SELECT takh FROM auth WHERE id={$_adrs[0][1]}";
 	$query = mysqli_query($conn, $q);
 	$comm["ptn"] = @mysqli_fetch_assoc($query)["takh"];
 	
-	/* Poem title */
 	$tbl = "tbl{$_adrs[0][1]}_{$_adrs[1][1]}";
-	$q = "select name from {$tbl} where id={$_adrs[2][1]}";
+	$q = "SELECT name FROM {$tbl} WHERE id={$_adrs[2][1]}";
 	$query = mysqli_query($conn, $q);
 	$comm["pmn"] = @mysqli_fetch_assoc($query)["name"];
-
-	/* Replace old comments by new ones */
+	
 	$comms[$key] = $comm;
 }
+
 mysqli_close($conn);
 
-/* Print the result */
 echo json_encode($comms);
 ?>
--- comments/index.php
+++ comments/index.php
@@ -1,22 +1,21 @@
 <?php
-include_once("../script/php/constants.php");
-include_once(ABSPATH . "script/php/colors.php");
-include_once(ABSPATH . "script/php/functions.php");
+require_once("../script/php/constants.php");
+require_once("../script/php/colors.php");
+require_once("../script/php/functions.php");
 
-$title = $_TITLE . " &rsaquo; پەراوێزی شیعرەکان";
+$title = $_TITLE . " › پەراوێزی شیعرەکان";
 $desc = "بیر و ڕای ئێوە سەبارەت بە شیعرەکان";
 $keys = $_KEYS;
 $t_desc = "";
 
-include(ABSPATH . "script/php/header.php");
+require_once("../script/php/header.php");
 
-/* Number of comments */
-$n = @filter_var($_GET['n'], FILTER_VALIDATE_INT) !== FALSE ?
-     $_GET['n'] : 20;
+/* User Input */
+$n = isset($_GET["n"]) ? intval($_GET["n"]) : 20;
+$from = isset($_GET["from"]) ? intval($_GET["from"]) : 0;
 ?>
 <style>
- .tmi-news
- {
+ .tmi-news {
 	 padding:0 .6em;
 	 font-size:1.1em;
  }
@@ -25,67 +24,74 @@ $n = @filter_var($_GET['n'], FILTER_VALIDATE_INT) !== FALSE ?
 	<h1 class="color-blue" style="font-size:1em;text-align:right">
 		پەراوێزی شیعرەکان
 	</h1>
-	<div class="tools-menu" style="font-size:.6em;padding-right:2em;margin-bottom:1em">
+	<div class="tools-menu"
+	     style="font-size:.6em;padding-right:2em;margin-bottom:1em">
 		<div style="display:flex;font-size:1.15em">
 			<div style="padding-left:1em">
 				ئەژمار:
 			</div>
 			<div>
-		<?php 
-		function print_tools_menu ($all, $sel)
-		{
-			foreach($all as $o)
-			{
-				$_ = num_convert($o, 'en', 'ckb');
-				
-				if($o == $sel)
-					echo "<span class='color-blue tmi-news'>{$_}</span>";
-				elseif($sel == -1 and $_ == 'هەموو')
-					echo "<span class='color-blue tmi-news'>هەموو</span>";
-				else
-				{
-					if($o == 'هەموو') $o = -1;
-					echo "<a href='?n=$o' class='tmi-news'>{$_}</a>";
+		<?php
+		function print_tools_menu($all, $sel) {
+			foreach($all as $o) {
+				$_ = num_convert($o, "en", "ckb");
+				if($o == $sel) {
+					echo "<span class='color-blue " .
+					     "tmi-news'>{$_}</span>";
+				}
+				elseif($sel == -1 and $_ == "هەموو") {
+					echo "<span class='color-blue " .
+					     "tmi-news'>هەموو</span>";
+				}
+				else {
+					if($o == "هەموو")
+						$o = -1;
+					echo "<a href='?n=$o' " .
+					     "class='tmi-news'>{$_}</a>";
 				}
 			}
 		}
-
-		print_tools_menu(['70','35','20','هەموو'], $n);
+		print_tools_menu(["70", "35", "20", "هەموو"], $n);
 		?>
 			</div>
 		</div>
 	</div>
 	<div id="result"></div>
-	<script>
-	 function loadComments (n) {
-		 const result = document.getElementById("result");
-		 result.innerHTML = "<div class='loader'></div>";
-		 getUrl(`get-comments.php?n=${n}`, function (resp) {
-			 resp = JSON.parse(resp);
-			 let accum = "";
-			 for(const comment of resp) {
-				 accum += `<div class='comment'><div class='comm-name'
->${comment.name}<span 
-style='font-size:.7em'> سەبارەت بە شیعری </span><a
-style='font-size:.75em;padding:0 .3em' 
-href='${_R + comment.address}'><i class='color-blue'>${comment.ptn}</i
-> &rsaquo; <i class='color-blue'>${comment.pmn}</i></a
-><span style='font-size:.7em'> نووسیویەتی:</span></div
-><div class='comm-body'>${comment.comment}</div><div class='comm-footer'
->${comment.date}</div></div>`;
-			 }
-			 result.innerHTML = accum;
-		 });
-	 }
-	 <?php if(! $no_head) { ?>
-	 window.addEventListener("load", function () {
-	 <?php } ?>
-		 loadComments("<?php echo $n; ?>");
-		 <?php if(! $no_head) { ?>
-	 });
-		 <?php } ?>
-	</script>
 </div>
+<script>
+ <?php
+ if(!$no_head)
+	 echo "window.addEventListener('load', () => {";
+ ?>
+ 
+ loadComments(<?php echo "'$n', '$from'" ?>)
+ 
+ <?php
+ if(!$no_head)
+	 echo "})";
+ ?>
+ 
+ function loadComments(n, from) {
+	 const result = document.getElementById('result')
+	 result.innerHTML = '<div class="loader"></div>'
+	 getUrl(`get-comments.php?n=${n}&from=${from}`, resp => {
+		 resp = JSON.parse(resp)
+		 const html = resp.map(commentToHtml).reduce((x, y) => x + y)
+		 result.innerHTML = html
+	 })
+ }
+ 
+ function commentToHtml(c) {
+	 return `<div class="comment"><div class="comm-name">` +
+		`${c.name}<span style="font-size:.7em"> سەبارەت بە شیعری ` +
+		`</span><a style="font-size:.75em;padding:0 .3em" href="` +
+		`${_R + c.address}"><i class="color-blue">${c.ptn}</i> › ` +
+		`<i class="color-blue">${c.pmn}</i></a><span style="` +
+		`font-size:.7em"> نووسیویەتی:</span></div><div class="` +
+		`comm-body">${c.comment}</div><div class="comm-footer">` +
+		`${c.date}</div></div>`
+ }
+</script>
 <?php
-include_once(ABSPATH . 'script/php/footer.php');
+require_once("../script/php/footer.php");
 ?>
