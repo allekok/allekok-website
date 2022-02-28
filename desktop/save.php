84e573003569c4a47fc65c50fbddb9aa0b22756b
Payam <payambapiri.97@gmail.com>
2022-02-28 17:09:44 +0330

    desktop/save.php: Clean up code

--- desktop/save.php
+++ desktop/save.php
@@ -1,5 +1,6 @@
 <?php
 require_once("../script/php/functions.php");
-echo isset($_POST['txt']) ?
-     save_QA($_POST['txt']) : die();
+echo isset($_POST["txt"]) ?
+     save_QA($_POST["txt"]) :
+     die();
 ?>
