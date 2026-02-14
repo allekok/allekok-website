21e91147758344f66b6400f03fd0f1f7a4e8bc1d
Payam <payambapiri.97@gmail.com>
2026-02-14 23:23:15 +0330

    [script/php/admin/set-pitew.php]: Fix a bug that has been wasting my time for so long. Because of this bug I couldn't write Kurdish script in description of "وەستای شیعر" file.

--- script/php/admin/set-pitew.php
+++ script/php/admin/set-pitew.php
@@ -10,11 +10,11 @@ $status = filter_var($_GET["status"], FILTER_VALIDATE_INT) !== false ?
 $adrs = @filter_var(trim($_GET["adrs"]), FILTER_SANITIZE_STRING);
 $desc = @filter_var(trim($_GET["desc"]), FILTER_SANITIZE_STRING);
 
-$statusObj = json_encode([
+$statusObj = addslashes(json_encode([
 	"status" => $status,
 	"url" => $adrs,
 	"desc" => $desc
-]);
+]));
 
 $q = "UPDATE pitew SET status='$statusObj' WHERE id=$id";
 require_once("../condb.php");
